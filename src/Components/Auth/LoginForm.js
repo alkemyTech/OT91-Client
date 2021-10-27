fd5e325934421589d5d66d5ab8991bfb9361b8af
ArielSurco <arielsurcoa@gmail.com>
2021-10-27 00:59:17 -0300

    Create validateLogin function

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,12 +1,36 @@
 import { useFormik } from 'formik';
 import '../FormStyles.css';
 
+const hasValue = (data, value) => data[value] && data[value].lenght > 0
+const isValidValue = (regExp, value) => regExp.test(value)
+
+const validateLogin = formData => {
+    const errors = {};
+    const emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    const passwordRegExp = /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[a-z\d@$!%*#?&]{6,}$/i;
+
+    if(!hasValue(formData, 'email'))
+        errors.email = 'This field cannot be blank';
+    else if(!isValidValue(emailRegExp, formData.email))
+        errors.email = 'Invalid email';
+
+    if(!hasValue(formData, 'password'))
+        errors.password = 'This field cannot be blank';
+    else if(formData.value < 6)
+        errors.password = 'Password must contain at least 6 characters'
+    else if(!isValidValue(passwordRegExp, formData.password))
+        errors.password = 'Password must include each of the following character types: letter, number, symbol';
+
+    return errors;
+}
+
 const LoginForm = () => {
     const formik = useFormik({
         initialValues: {
             email: '',
             password: ''
-        }
+        },
+        validate: validateLogin
     })
 
     const handleSubmit = (e) => {
