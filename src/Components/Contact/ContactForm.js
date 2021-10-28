b6541b542d55687f0cf6f81d0f974d437c34e0be
Telma <telmakn@gmail.com>
2021-10-28 18:14:15 -0300

    Input data validation

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -15,7 +15,26 @@ const ContactForm = () => {
         }}
         validate={(values) => {
           let errors = {}
+          if (!values.name) {
+            errors.name = 'Debes poner tu nombre'
+          }
 
+          if (!values.email) {
+            errors.email = 'Debes poner tu email'
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'La dirección de Email no es valida'
+          }
+          if (!values.phone) {
+            errors.phone = 'Debes poner tu número de telefono'
+          }
+          if (values.phone.length < 8) {
+            errors.phone = 'El numero de teléfono no es válido'
+          }
+          if (!values.message) {
+            errors.message = 'Debes escribir un mensaje'
+          }
           return errors
         }}
         onSubmit={(values, { resetForm }) => {
