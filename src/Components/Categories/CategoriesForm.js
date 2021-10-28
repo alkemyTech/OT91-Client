02b2bf2bd1fef3d03496738f59840a0c19718237
Telma <telmakn@gmail.com>
2021-10-28 11:48:04 -0300

    Formik, Material and CkEditor implementation

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,48 +1,196 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Container, TextField, Typography, Button } from '@material-ui/core'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import '../FormStyles.css'
 
 const CategoriesForm = () => {
-  const [initialValues, setInitialValues] = useState({
-    name: '',
-    description: '',
-  })
-
-  const handleChange = (e) => {
-    if (e.target.name === 'name') {
-      setInitialValues({ ...initialValues, name: e.target.value })
-    }
-    if (e.target.name === 'description') {
-      setInitialValues({ ...initialValues, description: e.target.value })
-    }
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(initialValues)
-  }
+  const [valuesForm, setValuesForm] = useState('')
 
   return (
-    <form className='form-container' onSubmit={handleSubmit}>
-      <input
-        className='input-field'
-        type='text'
-        name='name'
-        value={initialValues.name}
-        onChange={handleChange}
-        placeholder='Title'
-      ></input>
-      <input
-        className='input-field'
-        type='text'
-        name='description'
-        value={initialValues.description}
-        onChange={handleChange}
-        placeholder='Write some description'
-      ></input>
-      <button className='submit-btn' type='submit'>
-        Send
-      </button>
-    </form>
+    <Container
+      style={{
+        fontFamily: 'arial',
+      }}
+      className='form-container'
+    >
+      <Formik
+        initialValues={{
+          name: valuesForm.name ? valuesForm.name : '',
+          description: valuesForm.description ? valuesForm.description : '',
+          image: valuesForm.image ? valuesForm.image : '',
+        }}
+        validate={(values) => {
+          let errors = {}
+          if (!values.name) {
+            errors.name = 'Debes ponerle un Nombre a la Categoria'
+          }
+          if (values.name.length < 4) {
+            errors.name =
+              'El nombre de la Categoria debe tener mas de 4 caracteres'
+          }
+          if (!values.description) {
+            errors.description = 'Debes poner una descripción de la Categoria'
+          }
+          if (!values.image) {
+            errors.image = 'Debes poner una imagen para tu Categoria'
+          }
+          return errors
+        }}
+        onSubmit={(values, { resetForm }) => {
+          resetForm()
+        }}
+      >
+        {({ errors, setFieldValue }) => (
+          <Form className='form'>
+            <div>
+              <Typography
+                variant='h6'
+                style={{
+                  fontWeight: 'bold',
+                  margin: '1rem auto',
+                }}
+              >
+                Nombre de la Categoria
+              </Typography>
+              <TextField
+                fullWidth
+                id='fullWidth'
+                variant='outlined'
+                type='text'
+                name='name'
+                placeholder='Escribe el nombre'
+              />
+
+              <ErrorMessage
+                name='name'
+                component={() => (
+                  <Typography
+                    variant='subtitle2'
+                    color='error'
+                    style={{
+                      fontStyle: 'italic',
+                      fontWeight: 'bold',
+                      marginTop: '0.3rem',
+                      marginLeft: '0.2rem',
+                    }}
+                  >
+                    {errors.name}
+                  </Typography>
+                )}
+              />
+            </div>
+            <div>
+              <Typography
+                variant='h6'
+                style={{
+                  fontWeight: 'bold',
+                  margin: '1rem auto',
+                }}
+              >
+                Descripcion
+              </Typography>
+              <CKEditor
+                className='input-field'
+                config={{
+                  placeholder: 'Escribe una descripción',
+                }}
+                editor={ClassicEditor}
+                data=''
+                onChange={(e, editor) => {
+                  const data = editor.getData()
+                  setFieldValue('description', data)
+                }}
+              />
+              <ErrorMessage
+                name='description'
+                component={() => (
+                  <Typography
+                    variant='subtitle2'
+                    color='error'
+                    style={{
+                      fontStyle: 'italic',
+                      fontWeight: 'bold',
+                      marginTop: '0.3rem',
+                      marginLeft: '0.2rem',
+                    }}
+                  >
+                    {errors.description}
+                  </Typography>
+                )}
+              />
+            </div>
+            <div>
+              <Typography
+                variant='h6'
+                style={{
+                  fontWeight: 'bold',
+                  margin: '1rem auto',
+                }}
+              >
+                Image
+              </Typography>
+              <div>
+                {valuesForm.image > 0 && (
+                  <img
+                    src={values.image}
+                    alt='imagen vista previa'
+                    width='180'
+                    height='180'
+                  />
+                )}
+              </div>
+              <div>
+                <Field
+                  className='input-image'
+                  type='file'
+                  accept='image/png, image/jpg'
+                  id='image'
+                  name='image'
+                  value={undefined}
+                  onChange={(e) => {
+                    setFieldValue('image', e)
+                    const imageFile = e.target.files[0]
+                    const imageUrl = new FileReader()
+                    imageUrl.readAsDataURL(imageFile)
+                    imageUrl.onload = (e) => {
+                      setImg(e.target?.result)
+                    }
+                  }}
+                />
+              </div>
+              <ErrorMessage
+                name='image'
+                component={() => (
+                  <Typography
+                    variant='subtitle2'
+                    color='error'
+                    style={{
+                      fontStyle: 'italic',
+                      fontWeight: 'bold',
+                      marginTop: '0.3rem',
+                      marginLeft: '0.2rem',
+                    }}
+                  >
+                    {errors.image}
+                  </Typography>
+                )}
+              />
+            </div>
+
+            <Button
+              variant='contained'
+              size='medium'
+              type='submit'
+              color='primary'
+            >
+              Send
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Container>
   )
 }
 
