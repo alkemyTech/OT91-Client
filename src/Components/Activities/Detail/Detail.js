18b6a90d87fc4d8d3b8247d71142de4c2c9f77b2
Andrés Pereyra <andrespereyra.it@gmail.com>
2021-10-28 02:10:25 -0300

    Activity Detail Component with Styles in new Style folder. Add routing in App.js file

---
+++ src/Components/Activities/Detail/Detail.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { CardActionArea } from "@material-ui/core";
+
+import Title from "../../Title/Title";
+
+import "../../../Styles/CardStyle.css";
+
+const Detail = () => {
+  const { id } = useParams();
+
+  const [activity, setActivity] = useState("");
+
+  const [activityDesc, setActivityDesc] = useState("");
+
+  const stripedHtml = useCallback(() => {
+    typeof activity.description !== "undefined" &&
+      setActivityDesc(activity.description.replace(/<[^>]+>/g, ""));
+  }, [activity.description]);
+
+  useEffect(() => {
+    async function getUser() {
+      try {
+        const response = await axios.get(
+          `http://ongapi.alkemy.org/api/activities/${id}`
+        );
+        setActivity(response.data.data);
+        stripedHtml();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getUser();
+  }, []);
+
+  return (
+    <Card className="cardStyle">
+      <CardActionArea>
+        <CardMedia>
+          <Title
+            title={activity.name}
+            image={activity.image}
+            key={activity.id}
+          />
+        </CardMedia>
+        <CardContent>
+          <Typography>{activityDesc}</Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default Detail;
