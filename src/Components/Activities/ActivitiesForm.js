47193312f8129d7c99e200326e01ba4d20d10cff
Andreajhl <andreah29@famosos.com>
2021-10-28 00:59:39 -0300

    feat: :sparkles: add form for create and edit activities

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,32 +1,108 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React, {useEffect, useState} from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import {Formik, Form, Field,ErrorMessage} from 'formik';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-const ActivitiesForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        description: ''
-    });
+import {getDataForm, postDataForm, putDataForm} from './function';
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
-    }
+export default function ActivitiesForm({idActivity}){
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
-    
+
+  const [imgState,setImg] = useState('');
+  const [state, setstate] = useState('');
+
+  useEffect(() => {
+    if(idActivity) setstate(getDataForm(idActivity));
+  }, [idActivity]);
+
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Activity Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some activity description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
-export default ActivitiesForm;
+      <div className='div_form'>
+          <Formik
+              initialValues={state? {
+                name:state.name,
+                description:state.description,
+                image:state.image,
+              }:{
+                  name:'',
+                  description:'',
+                  image:'',
+              }}
+              validate={(valores)=>{
+                  let errors={}
+
+                  if(!valores.image) errors.image='You need to add a image';
+
+                  if(!valores.name) errors.name='You need to enter a name';
+                  if(valores.name.length<4) errors.name='The name must have more than 4 letters';
+                  
+                  if(!valores.description) errors.description='You need to add a description';
+                  
+                  return errors
+              }}
+              onSubmit={(values)=>{
+                const stringreplace =/'<p>'|['</p>']/gi;
+                values.description=values.description.replace(stringreplace,'');
+                idActivity ?  putDataForm( idActivity, values ) :postDataForm(values)
+              }}
+              
+          >
+              {({errors, setFieldValue,values})=>(
+                  <Form className='form' >
+                      <div className='form_div'>
+                          <p>Activity Name</p>
+                          <Field
+                            className="input-name" 
+                            type="text" 
+                            id='name'
+                            name="name"
+                            placeholder="Activity Name" 
+                          /> 
+                          <ErrorMessage name='name' component={()=><p className='form_div_error'>{errors.name}</p>}/>
+                      </div>
+
+                      <div className='form_div'>
+                          <p className='form_div_p'>Description</p>
+                          <CKEditor
+                              editor={ ClassicEditor }
+                              data=''
+                              config={ {
+                                  placeholder:"Write some activity description",
+                              } }
+                              onChange={( event, editor )=> {
+                                const data = editor.getData();
+                                setFieldValue("description", data)
+                              } }
+                          />
+                        <ErrorMessage name='description' component={()=><p >{errors.description}</p>}/> 
+                      </div>
+                      <div className='form_div'>
+                          <p className='form_div_p'>Image</p>
+                          <div>
+                            <Field
+                                className='input-image'
+                                type='file' 
+                                accept="image/png, image/jpg" 
+                                id='image'                                
+                                name='image' 
+                                value={undefined}
+                                onChange={(e) => {
+                                  setFieldValue("image",e)
+                                  const imageFile = e.target.files[0];
+                                  const imageUrl = new FileReader();
+                                  imageUrl.readAsDataURL(imageFile)
+                                  imageUrl.onload=(e)=>{
+                                      setImg(e.target?.result)
+                                  };
+                                }}
+                            /> 
+                            {imgState.length>0 && <img src={imgState} alt='imagen vista previa'  width='180' height='180'  />} 
+                          </div> 
+                          <ErrorMessage name='image' component={()=><p className='form_div_error'>{errors.image}</p>}/> 
+                      </div>
+                      <button className="submit-btn" type="submit">Send</button>
+                  </Form> 
+              )}
+          </Formik>
+      </div>
+  );
+};
