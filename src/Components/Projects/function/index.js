9a1c86087d820369d56c6d68a8f1ad4511d15653
Andreajhl <andreah29@famosos.com>
2021-10-28 00:44:31 -0300

    feat: :sparkles: agrega funciones de post, get, put para projects

--- src/Components/Projects/function/index.js
+++ src/Components/Projects/function/index.js
@@ -4,20 +4,15 @@ const URL = 'http://ongapi.alkemy.org/api';
 
 export const getDataForm = async (id) => {
     let {data}= await axios.get(`${URL}/projects/${id}`);
-    return data
-}
+    return data;
+};
 
 export const putDataForm = async (id,body) => {
     let {data}= await axios.patch(`${URL}/projects/${id}`,body);
-    return data
-}
+    return data;
+};
 
 export const postDataForm = async (body) => {
-    try {
-        let data= await axios.post(`${URL}/projects`,body);
-        return data
-    } catch (error) {
-        return error
-    }
-
-}
+    let {data} = await axios.post(`${URL}/projects`,body);
+    return data;
+};
