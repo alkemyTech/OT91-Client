764aaacb3385af163d8c8dc1fef385c43af3ad0f
ArielSurco <arielsurcoa@gmail.com>
2021-10-28 10:41:14 -0300

    Change group number

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -2,17 +2,15 @@ import { useFormik } from 'formik';
 import '../FormStyles.css';
 
 const notHasValue = (data, value) => !(data[value] && data[value].length > 0)
-const isNotValidValue = (regExp, value) => !regExp.test(value)
+const notValidValue = (regExp, value) => !regExp.test(value)
 
 const validateLogin = formData => {
     const errors = {};
     const emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
     const passwordRegExp = /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[a-z\d@$!%*#?&]{6,}$/i;
+    const notValidEmail = notHasValue(formData, 'email') || notValidValue(emailRegExp, formData.email);
 
-    if(notHasValue(formData, 'email'))
-        errors.email = 'This field cannot be blank';
-    else if(isNotValidValue(emailRegExp, formData.email))
-        errors.email = 'Invalid email';
+    if(notValidEmail) errors.email = 'Email is required and should be a valid email';
 
     if(notHasValue(formData, 'password'))
         errors.password = 'This field cannot be blank';
@@ -31,9 +29,7 @@ const LoginForm = () => {
             password: ''
         },
         validate: validateLogin,
-        onSubmit: formData => {
-            const tempObj = formData;
-        }
+        onSubmit: formData => { }
     })
 
     return (
