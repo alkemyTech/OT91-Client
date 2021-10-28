4940864fb0b675e1481014b33021c2237017b386
andrespereyra-it <73605405+andrespereyra-it@users.noreply.github.com>
2021-10-28 18:21:24 -0300

    Update Title.js

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,45 +1,33 @@
 import { useState, useEffect } from "react";
 import Box from "@material-ui/core/Box";
 
-import DefaultImage from "../../assets/default.jpg";
+import DefaultImage from "../../Assets/Title/default.jpg";
 
-export default function Title(props) {
-  const imageMock =
-    "https://images.unsplash.com/photo-1458682625221-3a45f8a844c7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80";
-  const titleMock = "Title";
+import "../../Styles/BoxStyle.css";
+
+const Title = (props) => {
+  const { title, image, id } = props;
 
   const [titleImage, setTitleImage] = useState("");
 
   useEffect(() => {
-    !imageMock ? setTitleImage(DefaultImage) : setTitleImage(imageMock);
-  }, []);
+    const finalImage = image ? image : DefaultImage;
+    setTitleImage(finalImage);
+  }, [image]);
 
   return (
     <div>
       <Box
+        className="boxStyle"
+        key={id}
         sx={{
           backgroundImage: `url(${titleImage})`,
-          height: "200px",
-          width: "100%",
-          position: "relative",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontFamily: "Montserrat",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          fontSize: "48px",
-          lineHeight: "72px",
-          textAlign: "center",
-          letterSpacing: "-0.015em",
-          color: "#000000"
         }}
       >
-        {titleMock}
+        {title}
       </Box>
     </div>
   );
 }
+
+export default Title;
