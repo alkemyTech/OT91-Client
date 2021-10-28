9471e3e31c9c5266a54581b5daf06953b655853f
julian624 <julianperezcv@hotmail.com>
2021-10-28 16:24:39 -0300

    Solved some problems of image post in the slides form

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,46 +1,102 @@
+import axios from "axios";
 import React, { useState } from "react";
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
 import "../FormStyles.css";
+import Swal from 'sweetalert2'
 
-const SlidesForm = () => {
+const SlidesForm = ({slides}) => {
   const [initialValues, setInitialValues] = useState({
-    name: "",
-    description: "",
+    name: slides?.name ?? '',
+    description:slides?.description ?? '',
+    image:slides?.image ?? '',
+    order:slides?.order ?? '',
   });
-
-  const handleChange = (e) => {
-    if (e.target.name === "name") {
-      setInitialValues({ ...initialValues, name: e.target.value });
-    }
-    if (e.target.name === "description") {
-      setInitialValues({ ...initialValues, description: e.target.value });
-    }
-  };
-
+  const hasSlides = !!slides
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(initialValues);
   };
-
+  const handleData = () => {
+    if(!hasSlides){
+    axios({
+      method: "POST",
+      url: "http://ongapi.alkemy.org/api/slides",
+      data: {
+        name: initialValues.name,
+        description: initialValues.description,
+        image: initialValues.image,
+      },
+    })
+      .then((res) => {
+        if (initialValues.name.length>4) {
+          return console.log(res);
+        } else {
+          Swal.fire({
+            title: 'Error!',
+            text: 'No se puede tener menos de 4 caracteristicas',
+            icon: 'error',
+            confirmButtonText: 'Ok, voy a editarlo!'
+          })
+        }
+      })
+      .catch((err) => console.log(err));
+    } else { axios({
+      method: "PUT",
+      url: `http://ongapi.alkemy.org/api/slides/${slides.id}`,
+      data: {
+        name: initialValues.name,
+        description: initialValues.description,
+        image: initialValues.image,
+      },
+    })}
+  };
   return (
     <form className="form-container" onSubmit={handleSubmit}>
       <input
-        className="input-field"
-        type="text"
-        name="name"
+        className='input-field'
+        type='text'
+        name='name'
+        required
         value={initialValues.name}
-        onChange={handleChange}
-        placeholder="Slide Title"
+        onChange={(e) => {
+          setInitialValues({
+            ...initialValues,
+            name: e.target.value,
+          })
+        }}
+        placeholder='Title (min 4 caracters)'
       ></input>
-      <input
-        className="input-field"
-        type="text"
-        name="description"
+      <CKEditor
+        editor={ClassicEditor}
+        className='input-field'
+        type='text'
+        name='description'
         value={initialValues.description}
-        onChange={handleChange}
-        placeholder="Write the description"
-      ></input>
-      <button className="submit-btn" type="submit">
-        Send
+        placeholder='Write some description'
+        required
+        onChange={(e, editor) => {
+          const data = editor.getData()
+          setInitialValues({ ...initialValues, description: data})
+        }}
+      />
+      <input
+        className='input-image'
+        type='file'
+        accept='image/png, image/jpg'
+        id='image'
+        name='image'
+        required
+        onChange={(e) => {
+          const imageFile = e.target.files[0]
+          const imageUrl = new FileReader()
+          imageUrl.readAsDataURL(imageFile)
+          imageUrl.onload = (e) => {
+            setInitialValues({...initialValues, image:e.target.result})
+          }
+        }}
+      />
+      <button onClick={handleData} className="submit-btn" type="submit">
+        {hasSlides ? "Editar" : "Enviar"}
       </button>
     </form>
   );
