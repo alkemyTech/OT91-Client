efaa30d50cd90c03b390b5d68b6f4986bc2c47b1
MarCortina <marmica24@hotmail.com>
2021-10-29 12:17:47 -0300

    add img category selected & onsubmit exec endpoint

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -1,41 +1,123 @@
-import React, { useState } from 'react';
-import '../../Components/FormStyles.css';
+import React, { useState, useEffect } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import "../../Components/FormStyles.css";
+import {
+  getCategories,
+  getNews,
+  updateNews,
+  createNews,
+} from "../../Services/newsFormApiService";
 
 const NewsForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        title: '',
-        content: '',
-        category: ''
-    });
+  const [initialValues, setInitialValues] = useState({
+    name: "",
+    content: "",
+    category: [],
+    id: "",
+    image: "",
+  });
+  const [categories, setCategories] = useState([]);
 
-    const handleChange = (e) => {
-        if(e.target.name === 'title'){
-            setInitialValues({...initialValues, title: e.target.value})
-        } if(e.target.name === 'content'){
-            setInitialValues({...initialValues, content: e.target.value})
-        } if(e.target.name === 'category') {
-            setInitialValues({...initialValues, category: e.target.value})
+  const getDataCategories = async (setCategories) => {
+    await getCategories(setCategories);
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === "name") {
+      setInitialValues({ ...initialValues, name: e.target.value });
+    }
+    if (e.target.name === "category") {
+      const newCategorySelected = categories?.find(
+        (category) => category.name === e.target.value
+      );
+      setInitialValues({
+        ...initialValues,
+        category: e.target.value,
+        id: newCategorySelected?.id,
+        image: newCategorySelected?.image,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postNews();
+  };
+
+  const postNews = async () => {
+    try {
+      const data = await getNews();
+      const sameData = data.find((news) => news.id === initialValues.id);
+      if (sameData) {
+        await updateNews(initialValues);
+      } else {
+        try {
+          await createNews(initialValues, setInitialValues);
+        } catch (err) {
+          console.log(err);
         }
+      }
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
+  useEffect(() => {
+    getDataCategories(setCategories);
+  }, []);
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="title" value={initialValues.title || ''} onChange={handleChange}></input>
-            <input className="input-field" type="text" name="content" value={initialValues.content || ''} onChange={handleChange}></input>
-            <select className="select-field" name="category" value={initialValues.category || ''} onChange={handleChange}>
-                <option value="" disabled>Select category</option>
-                <option value="1">Demo option 1</option>
-                <option value="2">Demo option 2</option>
-                <option value="3">Demo option 3</option>
-            </select>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
+  return (
+    <form className="form-container" onSubmit={handleSubmit}>
+      <label htmlFor="name">Title</label>
+      <input
+        className="input-field"
+        type="text"
+        name="name"
+        value={initialValues.name || ""}
+        onChange={handleChange}
+        minLength="5"
+        required
+      ></input>
+
+      <label htmlFor="content">Content</label>
+      <CKEditor
+        editor={ClassicEditor}
+        data={initialValues.content || ""}
+        required={true}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setInitialValues({ ...initialValues, content: data });
+        }}
+      />
+
+      <label htmlFor="category">Category</label>
+      <select
+        className="select-field"
+        name="category"
+        value={initialValues.category || ""}
+        onChange={handleChange}
+        required
+      >
+        <option value="" disabled>
+          Select Category
+        </option>
+        {categories?.map((category) => {
+          return (
+            <option key={category.id} value={category.name}>
+              {category.name}
+            </option>
+          );
+        })}
+      </select>
+      {initialValues.image ? (
+        <img src={initialValues.image} alt={initialValues.category}></img>
+      ) : null}
+      <button className="submit-btn" type="submit">
+        Send
+      </button>
+    </form>
+  );
+};
+
 export default NewsForm;
