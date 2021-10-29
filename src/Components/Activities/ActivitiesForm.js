9f64ad64b161c562ae4973259c20244561c0e0e4
Andreajhl <andreah29@famosos.com>
2021-10-28 23:05:16 -0300

    fix: :adhesive_bandage: modularization of the activity form
    it extracts in two functions the rendering of errors, and the obtaining of the url of the image inout

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,109 +1,110 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
+import {Formik, Form, Field} from 'formik';
+import { ErrorsForm } from '../common/messagesForm';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import {Formik, Form, Field,ErrorMessage} from 'formik';
+import { UrlInput } from '../common/getUrlInputFile';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
-import {getDataForm, postDataForm, putDataForm} from './function';
-
-export default function ActivitiesForm({idActivity}){
+import {getActivity,createActivity,modifiedActivity} from '../../Services/activityService';
 
 
-  const [imgState,setImg] = useState('');
-  const [state, setstate] = useState('');
+const ActivitiesForm = ({activityId}) => {
+
+
+  const [imgState,setImgState] = useState();
+  const [dataForm, setDataForm] = useState({
+    name:'',
+    description:'',
+    image:''
+  });
 
   useEffect(() => {
-    if(idActivity) setstate(getDataForm(idActivity));
-  }, [idActivity]);
+    if(activityId) setDataForm(getActivity(activityId));
+  }, [activityId]);
 
-    return (
-      <div className='form-container'>
-          <Formik
-              initialValues={state? {
-                name:state.name,
-                description:state.description,
-                image:state.image,
-              }:{
-                  name:'',
-                  description:'',
-                  image:'',
-              }}
-              validate={(valores)=>{
-                  let errors={}
+  const handleChaneCkEditor = (editor, setFieldValue) => {
+    setFieldValue("description", editor.getData())
+  };
 
-                  if(!valores.image) errors.image='You need to add a image';
+  const handleChangeinputFile = (e, setFieldValue) =>{ 
+    setFieldValue("image",e);
+    UrlInput(e, setImgState);
+  };
 
-                  if(!valores.name) errors.name='You need to enter a name';
-                  if(valores.name.length<4) errors.name='The name must have more than 4 letters';
-                  
-                  if(!valores.description) errors.description='You need to add a description';
-                  
-                  return errors
-              }}
-              onSubmit={(values)=>{
-                const stringreplace =/'<p>'|['</p>']/gi;
-                values.description=values.description.replace(stringreplace,'');
-                idActivity ?  putDataForm( idActivity, values ) :postDataForm(values)
-              }}
-              
-          >
-              {({errors, setFieldValue})=>(
-                  <Form className='form' >
-                      <div>
-                          <p>Activity Name</p>
-                          <Field
-                            className="input-field" 
-                            type="text" 
-                            id='name'
-                            name="name"
-                            placeholder="Activity Name" 
-                          /> 
-                          <ErrorMessage name='name' component={()=><p className='form_div_error'>{errors.name}</p>}/>
-                      </div>
-                      <div>
-                          <p>Description</p>
-                          <CKEditor
-                              editor={ ClassicEditor }
-                              data=''
-                              config={ {
-                                  placeholder:"Write some activity description",
-                              } }
-                              onChange={( event, editor )=> {
-                                const data = editor.getData();
-                                setFieldValue("description", data)
-                              } }
-                          />
-                        <ErrorMessage name='description' component={()=><p >{errors.description}</p>}/> 
-                      </div>
-                      <div >
-                          <p >Image</p>
-                          <div>
-                            {imgState.length>0 && <img src={imgState} alt='imagen vista previa'  width='180' height='180'  />} 
-                          </div>
-                          <div>
-                            <Field
-                                className='input-image'
-                                type='file' 
-                                accept="image/png, image/jpg" 
-                                id='image'                                
-                                name='image' 
-                                value={undefined}
-                                onChange={(e) => {
-                                  setFieldValue("image",e)
-                                  const imageFile = e.target.files[0];
-                                  const imageUrl = new FileReader();
-                                  imageUrl.readAsDataURL(imageFile)
-                                  imageUrl.onload=(e)=>{
-                                      setImg(e.target?.result)
-                                  };
-                                }}
-                            /> 
-                          </div> 
-                          <ErrorMessage name='image' component={()=><p className='form_div_error'>{errors.image}</p>}/> 
-                      </div>
-                      <button className="submit-btn" type="submit">Send</button>
-                  </Form> 
-              )}
-          </Formik>
-      </div>
+
+  return (
+    <div className='form-container'>
+      <Formik
+        initialValues={{
+          ...dataForm
+        }}
+        validate={(values)=>{
+            let errors={}
+
+            if(!values.image) errors.image='You need to add a image';
+
+            if(!values.name) errors.name='You need to enter a name';
+            if(values.name.length<4) errors.name='The name must have more than 4 letters';
+            
+            if(!values.description) errors.description='You need to add a description';
+            
+            return errors
+        }}
+        onSubmit={(values)=>{
+          values.description=values.description.replace(/'<p>'|['</p>']/gi,'');
+          activityId ?   modifiedActivity( activityId, values ) : createActivity(values)
+        }}
+      >
+        {({errors, setFieldValue})=>(
+          <Form className='form' >
+            <div>
+              <p>Activity Name</p>
+              <Field
+                className="input-field" 
+                type="text" 
+                id='name'
+                name="name"
+                placeholder="Activity Name" 
+              /> 
+              {errors.name && ErrorsForm ('title',errors.name)}
+            </div>
+            <div>
+              <p>Description</p>
+              <CKEditor
+                  editor={ ClassicEditor }
+                  data=''
+                  config={ {
+                      placeholder:"Write some activity description",
+                  } }
+                  onChange={(event, editor)=>handleChaneCkEditor(editor,setFieldValue)}
+              />
+              {errors.description && ErrorsForm ('description',errors.description)}
+            </div>
+            <div >
+              <p >Image</p>
+              <div>
+                {imgState && <img src={imgState} alt='imagen vista previa' width='180' height='180' />} 
+              </div>
+              <div>
+                <Field
+                    className='input-image'
+                    type='file' 
+                    accept="image/png, image/jpg" 
+                    id='image'                                
+                    name='image' 
+                    value=''
+                    onChange={(e)=>{
+                      handleChangeinputFile(e, setFieldValue)
+                    }}
+                /> 
+              </div> 
+              {errors.image && ErrorsForm ('image',errors.image)}
+            </div>
+            <button className="submit-btn" type="submit">Send</button>
+          </Form> 
+        )}
+      </Formik>
+    </div>
   );
 };
+
+export default ActivitiesForm;
