fc778080200c0e8634323a29891d5d00beed4c9d
ArielSurco <arielsurcoa@gmail.com>
2021-10-29 05:03:16 -0300

    Add form temporal structure

--- src/Components/Organization/DataEditForm.js
+++ src/Components/Organization/DataEditForm.js
@@ -1,5 +1,23 @@
+import { ShowFormControl } from './ShowFormControl';
+import { ShowFileInput } from './ShowFileInput';
+import { useState } from 'react';
+import '../FormStyles.css';
+import { ShowCKEditorInput } from './ShowCKEditorInput';
+
 const DataEditForm = () => {
-    return <p>Data edit form</p>
+    const [value, setValue] = useState('');
+
+    return (
+        <form className="form-container">
+            <ShowFormControl name="name" label="Organization name" type="text" value="" placeholder="Ingrese el nombre de la organización"/>
+            <ShowCKEditorInput label="Short description"/>
+            <ShowFileInput btnText="Subir logo" name="logo" value={value} onChange={(e) => setValue(e.target.files[0])} color="primary" accept=".png, .jpg"/>
+            <ShowFormControl name="longDescription" label="Descripcion larga" type="text" value="" placeholder="Escriba la descripción larga" />
+            <ShowFormControl name="facebook" label="Facebook" type="text" value="" placeholder="Ingrese la url"/>
+            <ShowFormControl name="instagram" label="Instagram" type="text" value="" placeholder="Ingrese la url"/>
+            <button className="submit-btn" type="submit">Guardar cambios</button>
+        </form>
+    )
 }
 
 export default DataEditForm;
