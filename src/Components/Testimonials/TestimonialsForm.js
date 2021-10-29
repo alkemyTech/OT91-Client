4a3095a813d709e50ed41a9b41f945815eec37f9
Andreajhl <andreah29@famosos.com>
2021-10-29 01:33:57 -0300

    feat: :sparkles: add form for edit and create testimonials
    it extracts in two functions the rendering of errors, and the obtaining of the url of the image inout

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,32 +1,110 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import {useEffect, useState} from 'react';
+import {Formik, Form, Field} from 'formik';
+import { ErrorsForm } from '../common/messagesForm';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import { UrlInput } from '../common/getUrlInputFile';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {getTestimonial,createTestimonial,modifiedTestimonial} from '../../Services/testimonialService';
 
-const TestimonialForm = () => {
-    const [initialValues, setInitialValues] = useState({
-       name: '',
-       description: '' 
-    });
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
-    }
+const TestimonialForm = ({testimonialId}) => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Testimonial Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Testimonial description"></input>
+  const [imgState,setImgState] = useState();
+  const [dataForm, setDataForm] = useState({
+    name:'',
+    description:'',
+    image: ''
+  });
+
+  useEffect(() => {
+    if(testimonialId) setDataForm(getTestimonial(testimonialId));
+  }, [testimonialId]);
+
+  const handleChaneCkEditor = (editor, setFieldValue) => {
+    setFieldValue("description", editor.getData())
+  };
+
+  const handleChangeinputFile = (e, setFieldValue) =>{ 
+    UrlInput(e, setImgState,setFieldValue);
+  };
+
+
+  return (
+    <div className='form-container'>
+      <Formik
+        initialValues={{
+          ...dataForm
+        }}
+        validate={(values)=>{
+            let errors={}
+
+            if(!values.image) errors.image='You need to add a image';
+
+            if(!values.name) errors.name='You need to enter a name';
+            if(values.name.length<4) errors.name='The name must have more than 4 letters';
+            
+            if(!values.description) errors.description='You need to add a description';
+            
+            return errors
+        }}
+        onSubmit={(values)=>{
+          values.description=values.description.replace(/'<p>'|['</p>']/gi,'');
+          values.image= imgState
+          testimonialId ?   modifiedTestimonial( testimonialId, values ) : createTestimonial(values)
+        }}
+      >
+        {({errors, setFieldValue,values})=>(
+          <Form className='form' >
+            <div>
+              <p>Testimonial Name</p>
+              <Field
+                className="input-field" 
+                type="text" 
+                id='name'
+                name="name"
+                placeholder="Testimonial Name" 
+              /> 
+              {errors.name && ErrorsForm ('title',errors.name)}
+            </div>
+            <div>
+              <p>Description</p>
+              <CKEditor
+                  editor={ ClassicEditor }
+                  data=''
+                  config={ {
+                      placeholder:"Write some testimonial description",
+                  } }
+                  onChange={(event, editor)=>handleChaneCkEditor(editor,setFieldValue)}
+              />
+              {errors.description && ErrorsForm ('description',errors.description)}
+            </div>
+            <div >
+              <p >Image</p>
+              <div>
+                {imgState && <img src={imgState} alt='imagen vista previa' width='180' height='180' />} 
+              </div>
+              <div>
+                <Field
+                    className='input-image'
+                    type='file' 
+                    accept="image/png, image/jpg" 
+                    id='image'                                
+                    name='image' 
+                    value=''
+                    onChange={(e)=>{ 
+                        handleChangeinputFile(e,setFieldValue)
+                    }}
+                /> 
+              </div> 
+              {errors.image && ErrorsForm ('image',errors.image)}
+            </div>
             <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
+          </Form> 
+        )}
+      </Formik>
+    </div>
+  );
+};
+
 export default TestimonialForm;
