dbeeee082549d587ff12a00539f522df5dbd5e6a
Andreajhl <andreah29@famosos.com>
2021-10-31 02:26:08 -0300

    feat: :sparkles: add function to render errors in the form

---
+++ src/Components/common/CustomErrorMessage.js
@@ -0,0 +1,11 @@
+
+import {ErrorMessage} from 'formik';
+
+export const CustomErrorMessage = (name,errors) =>{
+  return (
+    <ErrorMessage 
+      name={name} 
+      component={()=><p>{errors}</p>}
+    />
+  )
+}
