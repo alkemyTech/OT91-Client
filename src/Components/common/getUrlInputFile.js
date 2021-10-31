6005b87ef0defe3b3ae311c6caac80aecd6a1ae0
Andreajhl <andreah29@famosos.com>
2021-10-31 02:20:25 -0300

    feat: :fire: remove and delete de function in folder common

--- src/Components/common/getUrlInputFile.js
+++ src/Components/common/getUrlInputFile.js
@@ -1,12 +1,11 @@
 
-export const UrlInput = (e,setUrl,setFieldValue) =>{
+export const UrlInput = (e,setFieldValue) =>{
 
   const imageFile = e.target.files[0];
   const imageUrl = new FileReader();
   imageUrl.readAsDataURL(imageFile)
   imageUrl.onload=(e)=>{
     setFieldValue("image",e.target.result)
-    setUrl(e.target.result)
   }
 
 }
