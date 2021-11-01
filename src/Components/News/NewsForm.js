619cb66e4f99dca50a9a7c90dc059e3fc748f6d1
MarCortina <marmica24@hotmail.com>
2021-11-01 13:49:35 -0300

    changes names & business logic required

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -1,70 +1,60 @@
 import React, { useState, useEffect } from "react";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import "../../Components/FormStyles.css";
-import {
-  getCategories,
-  getNews,
-  updateNews,
-  createNews,
-} from "../../Services/newsFormApiService";
+import { createOrUpdateNews } from "../../Services/newsServices";
+import { getCategory } from "../../Services/categoriesServices";
+import InputImg from "../Inputs/InputImg";
+import InputEditor from "../Inputs/InputEditor";
 
 const NewsForm = () => {
-  const [initialValues, setInitialValues] = useState({
+  const [news, setNews] = useState({
     name: "",
     content: "",
     category: [],
-    id: "",
+    categoryId: "",
     image: "",
   });
   const [categories, setCategories] = useState([]);
 
-  const getDataCategories = async (setCategories) => {
-    await getCategories(setCategories);
-  };
-
   const handleChange = (e) => {
-    if (e.target.name === "name") {
-      setInitialValues({ ...initialValues, name: e.target.value });
-    }
-    if (e.target.name === "category") {
-      const newCategorySelected = categories?.find(
-        (category) => category.name === e.target.value
-      );
-      setInitialValues({
-        ...initialValues,
-        category: e.target.value,
-        id: newCategorySelected?.id,
-        image: newCategorySelected?.image,
-      });
+    switch (e.target.name) {
+      case "name":
+        setNews({ ...news, name: e.target.value });
+        break;
+      case "content":
+        setNews({ ...news, content: e.target.value });
+        break;
+      case "category":
+        const newCategorySelected = categories?.find(
+          (category) => category.name === e.target.value
+        );
+        setNews({
+          ...news,
+          category: e.target.value,
+          categoryId: newCategorySelected?.id,
+        });
+        break;
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    postNews();
+    sendNews();
   };
 
-  const postNews = async () => {
-    try {
-      const data = await getNews();
-      const sameData = data.find((news) => news.id === initialValues.id);
-      if (sameData) {
-        await updateNews(initialValues);
-      } else {
-        try {
-          await createNews(initialValues, setInitialValues);
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    } catch (err) {
-      console.log(err);
-    }
+  const sendNews = async () => {
+    createOrUpdateNews(news);
+    setNews({
+      name: "",
+      content: "",
+      category: [],
+      categoryId: "",
+      image: "",
+    });
   };
 
-  useEffect(() => {
-    getDataCategories(setCategories);
+  useEffect(async () => {
+    const data = await getCategory();
+    setCategories(data);
   }, []);
 
   return (
@@ -74,28 +64,17 @@ const NewsForm = () => {
         className="input-field"
         type="text"
         name="name"
-        value={initialValues.name || ""}
+        value={news.name || ""}
         onChange={handleChange}
         minLength="5"
         required
       ></input>
-
-      <label htmlFor="content">Content</label>
-      <CKEditor
-        editor={ClassicEditor}
-        data={initialValues.content || ""}
-        required={true}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          setInitialValues({ ...initialValues, content: data });
-        }}
-      />
-
+      <InputEditor news={news} setNews={setNews} />
       <label htmlFor="category">Category</label>
       <select
         className="select-field"
         name="category"
-        value={initialValues.category || ""}
+        value={news.category || ""}
         onChange={handleChange}
         required
       >
@@ -110,9 +89,7 @@ const NewsForm = () => {
           );
         })}
       </select>
-      {initialValues.image ? (
-        <img src={initialValues.image} alt={initialValues.category}></img>
-      ) : null}
+      <InputImg news={news} setNews={setNews} />
       <button className="submit-btn" type="submit">
         Send
       </button>
