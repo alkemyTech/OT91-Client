4668ad296e64599b6c99b5d183dad415aed2c74d
andrespereyra-it <73605405+andrespereyra-it@users.noreply.github.com>
2021-11-01 14:02:15 -0300

    Create EditItem.js

--- src/Components/Backoffice/Home/EditItem.js
+++ src/Components/Backoffice/Home/EditItem.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Button from "@material-ui/core/Button/Button";
 
-const EditItem = (props) => {
-  const { item, slidesIds, setSlidesIds } = props;
-
+const EditItem = ({ item, slidesIds, setSlidesIds }) => {
   const handleSelectImage = () => {
     slidesIds.length < 3
       ? setSlidesIds([...slidesIds, item.id])
@@ -12,29 +10,29 @@ const EditItem = (props) => {
 
   return (
     <>
-      <div>
-        <h5 className="card-title">{item && item.name}</h5>
-      </div>
-      <div>
-        <Button
-          variant="contained"
-          type="button"
-          className="btn btn-secondary"
-          value="Add to the Slider"
-          onClick={handleSelectImage}
-        >
-          Seleccionar Imagen
-        </Button>
-      </div>
-      <div>
-        <img
-          src={item && item.image}
-          alt={item && item.name}
-          key={item && item.id}
-        />
-      </div>
+      <h5 id="sliderImageName" name="sliderImageName" className="card-title">
+        {item && item.name}
+      </h5>
+      <Button
+        id="addToSliderButton"
+        name="addToSliderButton"
+        variant="contained"
+        type="button"
+        className="btn btn-secondary"
+        value="Add to the Slider"
+        onClick={handleSelectImage}
+      >
+        Seleccionar Imagen
+      </Button>
+      <img
+        id="sliderImage"
+        name="sliderImage"
+        src={item && item.image}
+        alt={item && item.name}
+        key={item && item.id}
+      />
     </>
   );
-}
+};
 
 export default EditItem;
