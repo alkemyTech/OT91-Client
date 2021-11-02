79db1ec9ec12829ec5388d89919da60897136215
ArielSurco <arielsurcoa@gmail.com>
2021-11-01 22:40:21 -0300

    Eslint corrections

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -15,7 +15,7 @@ const ActivitiesForm = () => {
       setInitialValues({ ...initialValues, description: e.target.value });
     }
   };
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(initialValues);
