0e6790a37e6f8a8efcc73ac903583cc0ebafb38c
Andreajhl <andreah29@famosos.com>
2021-11-02 01:56:45 -0300

    feat: :sparkles: add new request api rest to all activities
    add request get all

--- src/Services/activityService.js
+++ src/Services/activityService.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
+const URL = 'http://ongapi.alkemy.org/api'
+
 const getActivity = (id) => {
 
   const response = axios.get(`http://ongapi.alkemy.org/api/slides/${id}`);
   return response;
 };
+
+export const getAllActivities = async () =>{
+  let {data} =await axios.get(`${URL}/activities`)
+  return data
+}
 
 export default getActivity;
