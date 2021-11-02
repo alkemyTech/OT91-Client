dbbdec1cd095000840cac7677ddf0d376f940769
Andreajhl <andreah29@famosos.com>
2021-11-02 01:51:50 -0300

    feat: :sparkles: add component for render cards activities and title
    render component title and pagination activities

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,30 +1,13 @@
 import React from 'react';
+import Title from '../Title/Title'
+import PaginationActivities from './ActivitiesPagination/ActivitiesPagination';
 import '../CardListStyles.css';
-
 const ActivitiesList = () => {
-    const activitiesMock = [
-        {id: 2, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 1, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 3, name: 'Titulo de prueba', description: 'Descripcion de prueba'}
-    ];
 
     return (
         <div>
-            <h1>Listado Actividades</h1>
-            <ul className="list-container">
-                {activitiesMock.length > 0 ?
-                    activitiesMock.map((activity) => {
-                        return(
-                            <li className="card-info" key={activity.id}>
-                                <h3>{activity.name}</h3>
-                                <p>{activity.description}</p>
-                            </li>
-                        )
-                    })
-                :
-                    <p>No hay actividades</p>
-                }
-            </ul>
+            <Title title={'Listado Actividades'} id={'activitiesList'}/>
+            <PaginationActivities/>
         </div>
     );
 }
