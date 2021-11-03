8c51f4dbc192d1fb707401d7d108c1957d9ab4f7
Andreajhl <andreah29@famosos.com>
2021-11-03 00:59:28 -0300

    feat: :sparkles: add function for request private api rest
    the component receives the path, the body and the id, the 3 are required otherwise it returns an error

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+const URL = 'http://ongapi.alkemy.org/api'
+const token = localStorage.getItem('token');
+
+const headers = {
+    'Authorization': token,
+};
 
 const config = {
     headers: {
@@ -10,6 +16,16 @@ const Get = () => {
     axios.get('https://jsonplaceholder.typicode.com/users', config)
     .then(res => console.log(res))
     .catch(err => console.log(err))
+}
+
+
+
+export const privateRequestPut = async(path,id,body) => {
+    if (!path || !id || !body) {
+        throw new Error(`props not specified for async action put`);
+    }
+ let {data} = await axios.put(`${URL}/${path}/${id},`,headers,body)
+ return data
 }
 
 export default Get
