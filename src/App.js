75604953d172cc7f5d6db8e5f69c82c4d4bd5c11
Telma <telmakn@gmail.com>
2021-11-02 22:09:16 -0300

    Delete files that are not scope of the PR

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign'
 import ToysCampaign from './Campaigns/Toys/ToysCampaign'
 import MembersForm from './Components/Members/MembersForm'
 import ProjectsForm from './Components/Projects/ProjectsForm'
-import ContactForm from './Components/Contact/ContactForm'
 
 function App() {
   return (
@@ -22,14 +21,12 @@ function App() {
         <Switch>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
           <Route path='/create-activity' component={ActivitiesForm} />
-          <Route path='/create-category' component={CategoriesForm} />
           <Route path='/create-news' component={NewsForm} />
           <Route path='/backoffice/create-slide' component={SlidesForm} />
           <Route path='/create-testimonials' component={TestimonialForm} />
           <Route path='/create-user' component={UserForm} />
           <Route path='/create-member' component={MembersForm} />
           <Route path='/create-project' component={ProjectsForm} />
-          <Route path='/create-contact-message' component={ContactForm} />
           <Route path='/school-campaign' component={SchoolCampaign} />
           <Route path='/toys-campaign' component={ToysCampaign} />
         </Switch>
