96bc847ee29c29234774f91af5341feee8b8e498
Andreajhl <andreah29@famosos.com>
2021-11-03 17:07:25 -0300

    feat: :sparkles: contiene logica para agregar un paginado desde el front, asi se muestra solo 9 actividades por pantalla,
    This sets the items to be displayed per page in a local state and sends them to activities cards for them to go through.

---
+++ src/Components/Activities/ActivitiesPagination/ActivitiesPagination.js
@@ -0,0 +1,56 @@
+import {useState, useEffect} from "react"
+import ArrowLeft from '@material-ui/icons/ArrowLeft';
+import ArrowRight from '@material-ui/icons/ArrowRight';
+import ActivitiesCards from '../ActivitiesCards/ActivitiesCards';
+import {getAllActivities} from '../../../Services/activityService';
+const cards = 9;
+
+const ActivitiesPagination = () => {
+
+    const [items, setItems]=useState(null);
+    const [currentPag, setCurrentPag]= useState(0);
+    const [activities, setActivities] = useState([]);
+
+    useEffect(async() => {
+        let {data} = await getAllActivities()
+       setActivities(data)
+    }, []);
+
+    useEffect(()=>{
+        if(activities.length>1)setItems([...activities].splice(0,cards))
+    },[activities])
+
+    function setItemsPage(items,index,page){
+        setItems([...items].splice(index,cards))
+        setCurrentPag(page)
+    }
+
+    const next=()=>{
+        const totalElementos= activities.length
+        const next= currentPag +1
+        const index= next * cards
+        if(index>totalElementos) return;
+        setItemsPage(activities,index,next)
+    };
+
+    const prev=()=>{
+        const prev= currentPag-1
+        if(prev < 0) return;
+        const index= prev * cards
+        setItemsPage(activities,index,prev)
+    };
+
+    return (
+        <div className='paginacion'>
+            <div className='paginacion_activities'>
+                <ActivitiesCards activities={items} />
+            </div>
+          <div className='paginacion_buttons'>
+                <button className='paginacion_btn' onClick={()=>prev()}><ArrowLeft/></button>
+                <button className='paginacion_btn' onClick={()=>next()}><ArrowRight/></button>
+          </div>
+        </div>
+    )
+};
+
+export default ActivitiesPagination;
