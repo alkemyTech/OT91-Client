1f495dcadd33bb49c7256a97c4ae0aa058376f44
Andreajhl <andreah29@famosos.com>
2021-11-02 22:35:34 -0300

    feat: :sparkles: add function for replace "<p></p<"and  "<strong></strong>" to description card

--- src/Components/common/ckEditor/setCKEditorText.js
+++ src/Components/common/ckEditor/setCKEditorText.js
@@ -2,3 +2,6 @@ export const setCKEditorText = (values,field)=>{
     values[field]=values[field].replace(/'<p>'|['</p>']/gi,'');
     return values
 }
+export const replaceCKEditorText = (text)=>{
+    return text.replace(/'<p>'|['</p>']|<strong>|['</strong>']/gi,'');
+}
