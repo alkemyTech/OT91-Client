43a9004399239da97b4aa03162e2e0dbd00eaa30
Andreajhl <andreah29@famosos.com>
2021-11-03 17:25:18 -0300

    feat: :sparkles: add components to render card, show image title and description

---
+++ src/Components/Activities/ActivityCard/ActivityCard.js
@@ -0,0 +1,13 @@
+import {replaceCKEditorText} from '../../common/ckEditor/setCKEditorText'
+const ActivityCard = ({props}) => {
+    const {name,description,image}=props
+    return (
+        <div>
+            <img src={image} alt={name}/>
+            <h3>{name}</h3>
+            <p>{replaceCKEditorText(description)}</p>
+        </div>
+    )
+}
+
+export default ActivityCard;
