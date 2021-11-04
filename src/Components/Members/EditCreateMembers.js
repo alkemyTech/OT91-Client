90db0ab1ea2ab4e0f4e94fdb5be418a64b4d3986
MarCortina <marmica24@hotmail.com>
2021-11-04 09:22:34 -0300

    add component edit/create member

---
+++ src/Components/Members/EditCreateMembers.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import Swal from "sweetalert2";
+import "../FormStyles.css";
+
+const EditCreateMembers = () => {
+  const [member, setMember] = useState({
+    name: "",
+    description: "",
+    imageMember: "",
+    social_media: "",
+  });
+
+  const handleChange = (e) => {
+    if (e.target.name === "name") {
+      setMember({ ...member, name: e.target.value });
+    }
+
+    if (e.target.name === "image") {
+      const fileValid =
+        e.target.files[0].type === "image/jpg" ||
+        e.target.files[0].type === "image/png";
+      if (fileValid) {
+        setMember({ ...member, imageMember: e.target.value });
+      } else {
+        return Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Please select a valid image type jpg or png",
+        });
+      }
+    }
+    if (e.target.name === "social_media") {
+      setMember({ ...member, social_media: e.target.value });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (member.description === "") {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please fill in the description",
+      });
+    } else {
+      Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "Member has been created",
+      });
+      console.log(member);
+    }
+  };
+
+  return (
+    <form className="form-container" onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        className="input-field"
+        type="text"
+        name="name"
+        value={member.name}
+        onChange={handleChange}
+        placeholder="Name"
+        minLength="4"
+        required
+      ></input>
+      <label>Description</label>
+      <CKEditor
+        editor={ClassicEditor}
+        data={member.description}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setMember({ ...member, description: data });
+        }}
+      />
+      <label>Image</label>
+      <input
+        type="file"
+        onChange={handleChange}
+        name="image"
+        value={member.imageMember}
+        id="file-id"
+        accept="image/png, image/jpeg"
+        required
+      />
+      {member.imageMember && <img src={member.imageMember} alt="member" />}
+
+      <label>Social media</label>
+      <input
+        className="input-field"
+        type="url"
+        name="social_media"
+        value={member.social_media}
+        onChange={handleChange}
+        placeholder="url"
+        required
+      ></input>
+
+      <button className="submit-btn" type="submit">
+        Send
+      </button>
+    </form>
+  );
+};
+
+export default EditCreateMembers;
