cb30fef92c80b514f8696abd22f7738b7eada4cc
Martin Caparros <martincaparross@gmail.com>
2021-11-04 10:29:49 -0300

    Home folder, index.js, route created.

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import NewsDetailLayout from './Components/News/Detail/NewsDetailLayout';
-
+import Home from './Components/Home';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+          <Route exact path="/" component={Home} />
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
