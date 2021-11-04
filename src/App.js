62871aba331da12652b32c0e8d376625313ac510
ArielSurco <arielsurcoa@gmail.com>
2021-11-04 10:57:42 -0300

    Add /backoffice/news route that show NewsListEditTable component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Detail from "./Components/Activities/Detail/Detail";
 import OrganizationEditionForm from './Components/Organization/OrganizationEditionForm';
 import NewsDetailLayout from './Components/News/Detail/NewsDetailLayout';
 import Home from './Components/Home';
+import NewsListEditTable from "./Components/News/NewsListEditTable";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/organization/edit" component={OrganizationEditionForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
+          <Route path="/backoffice/news" component={NewsListEditTable} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
