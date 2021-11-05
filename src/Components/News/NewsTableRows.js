e4388ea70828d57fd71bf65c82ec7a773013a4c4
ArielSurco <arielsurcoa@gmail.com>
2021-11-05 19:32:11 -0300

    Apply feedback

--- src/Components/News/NewsTableRows.js
+++ src/Components/News/NewsTableRows.js
@@ -1,6 +1,6 @@
 import { TableRow, TableCell, IconButton } from "@material-ui/core";
 import { Edit, Delete } from "@material-ui/icons";
-import { showDate } from "../../Utils/formatters";
+import { formatDate } from "../../Utils/formatters";
 import '../../Styles/Table.css';
 
 const NewsTableRows = ({newsList, onEdit, onDelete}) => {
@@ -14,10 +14,10 @@ const NewsTableRows = ({newsList, onEdit, onDelete}) => {
                 <TableCell align="center">
                     <img className="table-row-image" src={news.image} alt={news.name}/>
                 </TableCell>
-                <TableCell align="center"> {showDate(news.createdAt)} </TableCell>
+                <TableCell align="center"> {formatDate(news.createdAt)} </TableCell>
                 <TableCell align="center">
-                    <IconButton onClick={onEdit}> <Edit/> </IconButton>
-                    <IconButton onClick={onDelete}> <Delete/> </IconButton>
+                    <IconButton onClick={() => onEdit(news.id)}> <Edit/> </IconButton>
+                    <IconButton onClick={() => onDelete(news.id)}> <Delete/> </IconButton>
                 </TableCell>
             </TableRow>
         ))}
