c2bcc79ddc689bf03fc37147eb5e17952b5220a4
Telma <telmakn@gmail.com>
2021-11-05 12:33:32 -0300

    Component creation and path

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ActivitiesForm from "./Components/Activities/ActivitiesForm";
-import CategoriesForm from "./Components/Categories/CategoriesForm";
-import NewsForm from "./Components/News/NewsForm";
-import SlidesForm from "./Components/Slides/SlidesForm";
-import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
-import UserForm from "./Components/Users/UsersForm";
-import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
-import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import MembersForm from "./Components/Members/MembersForm";
-import ProjectsForm from "./Components/Projects/ProjectsForm";
-import Detail from "./Components/Activities/Detail/Detail";
-import OrganizationEditionForm from './Components/Organization/OrganizationEditionForm';
-import NewsDetailLayout from './Components/News/Detail/NewsDetailLayout';
-import Home from './Components/Home';
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ActivitiesForm from './Components/Activities/ActivitiesForm'
+import CategoriesForm from './Components/Categories/CategoriesForm'
+import NewsForm from './Components/News/NewsForm'
+import SlidesForm from './Components/Slides/SlidesForm'
+import TestimonialForm from './Components/Testimonials/TestimonialsForm'
+import UserForm from './Components/Users/UsersForm'
+import SchoolCampaign from './Campaigns/School/SchoolCampaign'
+import ToysCampaign from './Campaigns/Toys/ToysCampaign'
+import MembersForm from './Components/Members/MembersForm'
+import ProjectsForm from './Components/Projects/ProjectsForm'
+import Detail from './Components/Activities/Detail/Detail'
+import OrganizationEditionForm from './Components/Organization/OrganizationEditionForm'
+import NewsDetailLayout from './Components/News/Detail/NewsDetailLayout'
+import Home from './Components/Home'
+import NewsList from './Components/News/NewsList'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/organization/edit" component={OrganizationEditionForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/edit-activity/:activityId" component={ActivitiesForm} />
-          <Route path="/edit-testimonial/:testimonialId" component={TestimonialForm} />
-          <Route path="/edit-project/:projectId" component={ProjectsForm} />
-          <Route path="/novedades/:id" component={NewsDetailLayout} />
-          <Route path="/activity-detail/:id" component={Detail} />
+          <Route exact path='/' component={Home} />
+          <Route path='/create-activity' component={ActivitiesForm} />
+          <Route path='/create-category' component={CategoriesForm} />
+          <Route path='/create-news' component={NewsForm} />
+          <Route path='/novedades' component={NewsList} />
+          <Route
+            path='/backoffice/organization/edit'
+            component={OrganizationEditionForm}
+          />
+          <Route path='/backoffice/create-slide' component={SlidesForm} />
+          <Route path='/create-testimonials' component={TestimonialForm} />
+          <Route path='/create-user' component={UserForm} />
+          <Route path='/create-member' component={MembersForm} />
+          <Route path='/create-project' component={ProjectsForm} />
+          <Route path='/school-campaign' component={SchoolCampaign} />
+          <Route path='/toys-campaign' component={ToysCampaign} />
+          <Route path='/edit-activity/:activityId' component={ActivitiesForm} />
+          <Route
+            path='/edit-testimonial/:testimonialId'
+            component={TestimonialForm}
+          />
+          <Route path='/edit-project/:projectId' component={ProjectsForm} />
+          <Route path='/novedades/:id' component={NewsDetailLayout} />
+          <Route path='/activity-detail/:id' component={Detail} />
         </Switch>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -1,32 +1,20 @@
-import React from 'react';
-import '../CardListStyles.css';
-
+import React from 'react'
+import '../CardListStyles.css'
 const NewsList = () => {
-    const newsMock = [
-        {id: 2, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 1, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 3, name: 'Titulo de prueba', description: 'Descripcion de prueba'}
-    ];
-
-    return (
-        <div>
-            <h1>Listado de Novedades</h1>
-            <ul className="list-container">
-                {newsMock.length > 0 ? 
-                    newsMock.map((element) => {
-                        return(
-                            <li className="card-info" key={element.id}>
-                                <h3>{element.name}</h3>
-                                <p>{element.description}</p>
-                            </li>
-                        )
-                    })
-                :
-                    <p>No hay novedades</p>
-                }
-            </ul>
-        </div>
-    );
+  const cardsFutureComponent = [{ id: 2 }, { id: 1 }, { id: 3 }]
+  return (
+    <div>
+      <h1>Novedades</h1>
+      <ul className='list-container'>
+        {cardsFutureComponent.length > 0 ? (
+          cardsFutureComponent.map((card) => {
+            return <li className='card-info' key={card.id}></li>
+          })
+        ) : (
+          <p>No hay novedades</p>
+        )}
+      </ul>
+    </div>
+  )
 }
- 
-export default NewsList;
+export default NewsList
