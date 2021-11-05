b832c81d012bf095d5afbb32d306d1938c2b7a92
Andreajhl <andreah29@famosos.com>
2021-11-04 23:10:38 -0300

    add component that renders the pagination with the cards and the title of the page

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import Title from '../Title/Title';
-import PaginationActivities from './ActivitiesPagination/ActivitiesPagination';
+
 import '../CardListStyles.css';
+import Title from '../Title/Title';
+import { useEffect, useState } from 'react';
+import {ACTIVITIES} from '../common/text/textActivity';
+import {getAllActivities} from '../../Services/activityService';
+import ActivitiesCards from './ActivitiesCards/ActivitiesCards';
+import ListPagination from '../common/ListPagination/ListPagination';
 const ActivitiesList = () => {
+
+    const [items, setItems] = useState([]);
+
+    useEffect(async() => {
+        const { data }  =  await getAllActivities();
+        setItems(data)
+    }, []);
+
+    const showItemsListComponent = (items) => (<ActivitiesCards activities={items} />);
 
     return (
         <div>
-            <Title title={'Actividades'} id={'activitiesList'}/>
-            <PaginationActivities/>
+            <Title title={ACTIVITIES} id={'activitiesList'}/>
+            <ListPagination items={items} showItemsListComponent={showItemsListComponent } />
         </div>
     );
 };
