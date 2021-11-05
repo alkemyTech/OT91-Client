63df8e5ebe802b426854c3f65ba8402a97fb84e2
Andreajhl <andreah29@famosos.com>
2021-11-04 23:09:20 -0300

    feat: :sparkles: add component to render cards
    use the ckeditor function to remove the html code from the description and use constants for texts, so they can be changed more easily in the future

--- src/Components/Activities/ActivitiesCards/ActivitiesCards.js
+++ src/Components/Activities/ActivitiesCards/ActivitiesCards.js
@@ -1,14 +1,15 @@
+import {NO_ACTIVITIES} from '../common/text/textActivity';
 import{setCKEditorText} from '../../common/ckEditor/setCKEditorText';
 const ActivitiesCards = ({activities}) => {
 
     return (
         <div>
-            {activities ? activities.map(e=> <div>
+            {activities.length ? activities.map(e=> <div>
                 <img src={e.image} alt={e.name}/>
                 <h3>{e.name}</h3>
                 <p>{setCKEditorText(e,'description').description}</p>
             </div>
-            ) : <p>No hay Actividades</p>
+            ) : <p>{NO_ACTIVITIES}</p>
             }
         </div>
     )
