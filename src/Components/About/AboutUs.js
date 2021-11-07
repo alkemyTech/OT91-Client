966abfd48aa9a96cfe72ac411493081224e46696
ArielSurco <arielsurcoa@gmail.com>
2021-11-07 20:33:07 -0300

    Remove AboutUsDescription component and add it in AboutUs

--- src/Components/About/AboutUs.js
+++ src/Components/About/AboutUs.js
@@ -1,11 +1,18 @@
-import AboutUsDescription from "./AboutUsDescription";
+import Title from "../Title/Title";
+import { Box } from "@material-ui/core";
+import '../../Styles/Container.css';
 
 const AboutUs = () => {
     const description = `Somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de:
         Educación, deportes, primera infancia, salud, alimentación y trabajo social.`
     return (
         <>
-            <AboutUsDescription description={description}/>
+            <Box className="title-container">
+                <Title title="Nosotros"/>
+                <div className="title-container-text">
+                    <p>{description}</p>
+                </div>
+            </Box>
         </>
     )
 }
