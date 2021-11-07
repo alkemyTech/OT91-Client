34f2c3890b3827a9b26fe5a76d62a25d7411ac7d
MarCortina <marmica24@hotmail.com>
2021-11-07 15:18:51 -0300

    resolve conflicts in app

--- src/App.js
+++ src/App.js
@@ -12,17 +12,12 @@ import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
 import MembersForm from "./Components/Members/MembersForm";
 import ProjectsForm from "./Components/Projects/ProjectsForm";
 import Detail from "./Components/Activities/Detail/Detail";
-import OrganizationEditionForm from "./Components/Organization/OrganizationEditionForm";
-import NewsDetailLayout from "./Components/News/Detail/NewsDetailLayout";
-import ActivitiesList from "./Components/Activities/ActivitiesList";
-import Home from "./Components/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
@@ -46,7 +41,6 @@ function App() {
           <Route path="/edit-project/:projectId" component={ProjectsForm} />
           <Route path="/novedades/:id" component={NewsDetailLayout} />
           <Route path="/activity-detail/:id" component={Detail} />
-          <Route path="/activities" component={ActivitiesList} />
         </Switch>
       </BrowserRouter>
     </>
