6b69e7f6299399ef1806719d0d5a8640ea004f08
ArielSurco <arielsurcoa@gmail.com>
2021-11-07 18:05:07 -0300

    Modify and modularize NewsListEditTable

--- src/Components/News/NewsListEditTable.js
+++ src/Components/News/NewsListEditTable.js
@@ -1,54 +1,33 @@
-import {Paper, TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Button, IconButton } from "@material-ui/core";
-import { Delete, Edit, AddCircle } from "@material-ui/icons";
+import {TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { NewsTableRows } from "./NewsTableRows";
+import { createNewsObject } from "../../Utils/formatters";
 
-const showDate = date => `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-const NewsListEditTable = () => {
-    const createNews = (name, image, createdAt) => ({name, image, createdAt});
-
-    const rows = [
-    createNews('Novedad 1', 'https://bit.ly/3GRp1al', new Date()),
-    createNews('Novedad 2', 'https://bit.ly/3GRp1al', new Date()),
-    createNews('Novedad 3', 'https://bit.ly/3GRp1al', new Date()),
+const newsList = [
+    createNewsObject('Novedad 1', 'https://bit.ly/3GRp1al', new Date()),
+    createNewsObject('Novedad 2', 'https://bit.ly/3GRp1al', new Date()),
+    createNewsObject('Novedad 3', 'https://bit.ly/3GRp1al', new Date()),
     ];
 
+const NewsListEditTable = () => {
     return (
     <>
     <TableContainer>
-        <Table size="small" sx={{ minWidth: 650}} aria-label="simple table">
+        <Table size="small" aria-label="simple table">
             <TableHead>
                 <TableRow>
-                    <TableCell>Nombre de Novedad</TableCell>
-                    <TableCell align="center">Imagen</TableCell>
-                    <TableCell align="center">Fecha de creacion</TableCell>
-                    <TableCell align="center" >
-                        <Button variant="contained" size="small" style={{marginLeft: 'auto'}} endIcon={<AddCircle />} component={Link} to="/backoffice/news/create" color="primary">
-                            Crear
+                    <TableCell>Name</TableCell>
+                    <TableCell align="center">Image</TableCell>
+                    <TableCell align="center">Created At</TableCell>
+                    <TableCell align="center">
+                        <Button variant="contained" size="small" component={Link} to="/backoffice/news/create" color="primary">
+                            Create News
                         </Button>
                     </TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
-            {rows.map((row) => (
-                <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                    <TableCell component="th" scope="row">
-                        {row.name}
-                    </TableCell>
-                    <TableCell align="center"> <img src={row.image} alt="" style={{maxHeight:'40px'}}/> </TableCell>
-                    <TableCell align="center">{showDate(row.createdAt)}</TableCell>
-                    <TableCell align="center">
-                        <IconButton>
-                            <Edit/>
-                        </IconButton>
-                        <IconButton>
-                            <Delete/>
-                        </IconButton>
-                    </TableCell>
-                </TableRow>
-            ))}
+                <NewsTableRows newsList={newsList} />
             </TableBody>
         </Table>
     </TableContainer>
