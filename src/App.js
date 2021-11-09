44208f6324f202b5703c3c89e4a308ab6ae46c6d
Martin Caparros <martincaparross@gmail.com>
2021-11-09 12:15:20 -0300

    Provider and authReducer created, all actions requested created: -Logout: you reset all the related data about user - setUserData: to storage all the user data. - setToken: to isolate the userToken - setIsLogged: so you can check if the user is or not logged correctly.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ActivitiesForm from "./Components/Activities/ActivitiesForm";
 import CategoriesForm from "./Components/Categories/CategoriesForm";
@@ -20,39 +21,42 @@ import MemberEditCreate from './Components/Members/MemberEditCreate';
 import Home from './Components/Home';
 import AboutUs from "./Components/About/AboutUs";
 function App() {
+
   return (
     <>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/members/edit" component={MemberEditCreate} />
-          <Route
-            path="/backoffice/organization/edit"
-            component={OrganizationEditionForm}
-          />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/edit-activity/:activityId" component={ActivitiesForm} />
-          <Route
-            path="/edit-testimonial/:testimonialId"
-            component={TestimonialForm}
-          />
-          <Route path="/edit-project/:projectId" component={ProjectsForm} />
-          <Route path="/novedades/:id" component={NewsDetailLayout} />
-          <Route path="/activity-detail/:id" component={Detail} />
-          <Route path="/backoffice" component={Backoffice} />
-          <Route path="/activities" component={ActivitiesList} />
-          <Route path="/nosotros" component={AboutUs} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/create-activity" component={ActivitiesForm} />
+            <Route path="/create-category" component={CategoriesForm} />
+            <Route path="/create-news" component={NewsForm} />
+            <Route path="/backoffice/members/edit" component={MemberEditCreate} />
+            <Route
+              path="/backoffice/organization/edit"
+              component={OrganizationEditionForm}
+            />
+            <Route path="/backoffice/create-slide" component={SlidesForm} />
+            <Route path="/create-testimonials" component={TestimonialForm} />
+            <Route path="/create-user" component={UserForm} />
+            <Route path="/create-member" component={MembersForm} />
+            <Route path="/create-project" component={ProjectsForm} />
+            <Route path="/school-campaign" component={SchoolCampaign} />
+            <Route path="/toys-campaign" component={ToysCampaign} />
+            <Route path="/edit-activity/:activityId" component={ActivitiesForm} />
+            <Route
+              path="/edit-testimonial/:testimonialId"
+              component={TestimonialForm}
+            />
+            <Route path="/edit-project/:projectId" component={ProjectsForm} />
+            <Route path="/novedades/:id" component={NewsDetailLayout} />
+            <Route path="/activity-detail/:id" component={Detail} />
+            <Route path="/backoffice" component={Backoffice} />
+            <Route path="/activities" component={ActivitiesList} />
+            <Route path="/nosotros" component={AboutUs} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 };
--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authReducer/authReducer';
 
 export default configureStore({
   reducer: {
+    authorization: authReducer,
   },
 });
