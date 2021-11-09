5617eaaf42328e25c01a196b69da81a18335993b
andrespereyra-it <73605405+andrespereyra-it@users.noreply.github.com>
2021-11-09 11:50:29 -0300

    Update membersService.js

--- src/Services/membersService.js
+++ src/Services/membersService.js
@@ -1,29 +1,31 @@
 import axios from "axios";
+import getAuthorizationHeader from "./privateApiService";
 
 const URL = "http://ongapi.alkemy.org/api";
+const authorizationHeader = getAuthorizationHeader();
 
 const getMembers = () => {
-  const response = axios.get(`${URL}/members`);
+  const response = axios.get(`${URL}/members`, authorizationHeader);
   return response;
 };
 
 const createMember = (data) => {
-  const response = axios.post(`${URL}/members`, data);
+  const response = axios.post(`${URL}/members`, data, authorizationHeader);
   return response;
 };
 
 const getMember = (id) => {
-  const response = axios.get(`${URL}/members/${id}`);
+  const response = axios.get(`${URL}/members/${id}`, authorizationHeader);
   return response;
 };
 
 const updateMember = (id, data) => {
-  const response = axios.put(`${URL}/members/${id}`, data);
+  const response = axios.put(`${URL}/members/${id}`, data, authorizationHeader);
   return response;
 };
 
 const removeMember = (id) => {
-  const response = axios.delete(`${URL}/members/${id}`);
+  const response = axios.delete(`${URL}/members/${id}`, authorizationHeader);
   return response;
 };
 
