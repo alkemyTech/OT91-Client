9f5d25138a3c1c333f90efa1735f3f680cb2644d
MarCortina <marmica24@hotmail.com>
2021-11-09 19:18:14 -0300

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ActivitiesForm from "./Components/Activities/ActivitiesForm";
-import CategoriesForm from "./Components/Categories/CategoriesForm";
-import NewsForm from "./Components/News/NewsForm";
-import SlidesForm from "./Components/Slides/SlidesForm";
-import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
-import UserForm from "./Components/Users/UsersForm";
-import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
-import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import MembersForm from "./Components/Members/MembersForm";
-import ProjectsForm from "./Components/Projects/ProjectsForm";
-import Detail from "./Components/Activities/Detail/Detail";
-import OrganizationEditionForm from "./Components/Organization/OrganizationEditionForm";
-import NewsDetailLayout from "./Components/News/Detail/NewsDetailLayout";
-import ActivitiesList from "./Components/Activities/ActivitiesList";
-import Home from "./Components/Home";
-import ActivitiesTable from "./Components/Backoffice/ActivitiesBackOffice/ActivitiesTable";
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/members/edit" component={MemberEditCreate} />
-          <Route path="/backoffice/activities" component={ActivitiesTable} />
           <Route
             path="/backoffice/organization/edit"
             component={OrganizationEditionForm}
