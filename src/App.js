a072f2ea14835b1e3287eca974aa59c338f88716
MarCortina <marmica24@hotmail.com>
2021-11-09 19:26:09 -0300

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ function App() {
     <>
       <BrowserRouter>
         <Switch>
-          <Route path="/backoffice/organization/edit" component={OrganizationEditionForm}/>
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/edit-activity/:activityId" component={ActivitiesForm} />
           <Route
             path="/edit-testimonial/:testimonialId"
             component={TestimonialForm}
