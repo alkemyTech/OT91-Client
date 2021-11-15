60c6b26af0562ee2abf55b74c9b722f3a550e8a8
Martin Caparros <martincaparross@gmail.com>
2021-11-15 09:38:13 -0300

    quotes fix

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import {Box} from '@mui/material";
+import {Box} from '@mui/material';
 import DefaultImage from "../../Assets/Title/default.jpg";
 
 import "../../Styles/BoxStyle.css";
