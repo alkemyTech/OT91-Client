70989cf007e0bbbeb67bc9b41edfa4b6aa3e2474
Telma <telmakn@gmail.com>
2021-11-16 00:09:26 -0300

    Adding loader and logic to components

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -1,55 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../Styles/CardStyle.css";
 import { listHasValues } from "../../Utils/validation";
 import Title from "../Title/Title";
 import CustomCard from "../Card/CustomCard";
 import { Container } from "@mui/material";
-const lastNewsMock = [
-  {
-    id: 626,
-    name: "VI Jornada recreativa",
-    content:
-      "Se realizó la VI jornada recreativa de invierno, jovenes de todas las edades participarion en juegos y los más chicos presentaron una coreografía espectacular!",
-    image: "https://ukrainer.net/wp-content/uploads/2019/11/11-99.jpg",
-  },
-  {
-    id: 629,
-    name: "Jornada domigo juguetes 12",
-    content:
-      "VI jornada recreativa de invierno, jovenes de todas las edades participarion en juegos y los m&aacute;s chicos presentaron una core",
-    image: "https://ukrainer.net/wp-content/uploads/2019/11/0-113.jpg",
-    user_id: null,
-  },
-  {
-    id: 637,
-    name: "Actividades anuales",
-    content:
-      "Jornada recreativa de invierno, jovenes de todas las edades participarion en juegos y los m&aacute;s chicos presentaron una core",
-    image:
-      "https://www.kleineherzen.or.at/wp-content/uploads/Kids_Mutter_Kind_haus2019.png",
-  },
-];
+import LoadingSpinner from "../../Utils/loadingSpinner";
+
 const NewsList = ({ lastNews }) => {
-  const newsListHasValues = listHasValues(lastNewsMock);
+  const [loading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    lastNews && setIsLoading(false);
+  }, [lastNews]);
+
+  const newsListHasValues = listHasValues(lastNews);
   return (
     <Container className="ContainerList">
-      <Title title="Novedades" />
-      <ul className="list-container">
-        {newsListHasValues ? (
-          lastNewsMock.map((news) => {
-            return (
-              <CustomCard
-                key={news.id}
-                title={news.name}
-                img={news.image}
-                description={news.content}
-              />
-            );
-          })
-        ) : (
-          <p>No hay novedades</p>
-        )}
-      </ul>
+      {loading ? (
+        <div className="spinner">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <div>
+          <Title title="Novedades" />
+          <ul className="list-container">
+            {newsListHasValues ? (
+              lastNews.map((news) => {
+                return (
+                  <CustomCard
+                    key={news.id}
+                    title={news.name}
+                    img={news.image}
+                    description={news.content}
+                  />
+                );
+              })
+            ) : (
+              <p>No hay novedades</p>
+            )}
+          </ul>
+        </div>
+      )}
     </Container>
   );
 };
