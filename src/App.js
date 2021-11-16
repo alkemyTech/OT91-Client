02930bbc6ab2c534de273222967bcfe6eaabf711
Andrés Pereyra <andrespereyra.it@gmail.com>
2021-11-16 18:22:38 -0300

    App.js conflicts solved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Provider } from 'react-redux';
-import store from './app/store'
+import { Provider } from "react-redux";
+import store from "./app/store";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ActivitiesForm from "./Components/Activities/ActivitiesForm";
 import CategoriesForm from "./Components/Categories/CategoriesForm";
@@ -21,7 +21,7 @@ import ActivitiesList from "./Components/Activities/ActivitiesList";
 import MemberEditCreate from "./Components/Members/MemberEditCreate";
 import Home from "./Components/Home";
 import AboutUs from "./Components/About/AboutUs";
-import ListSlides from './Components/Slides/ListSlides/ListSlides';
+import ListSlides from "./Components/Slides/ListSlides/ListSlides";
 import ContactForm from "./Components/Contact/ContactForm";
 import NewsListEditTable from "./Components/News/NewsListEditTable";
 import MembersResults from "./Components/Backoffice/Members/MembersResults";
@@ -30,50 +30,56 @@ import CategoriesList from "./Components/Backoffice/Categories/CategoriesList";
 import NewsList from "./Components/News/NewsList";
 import Donation from "./Components/Donations/Donation";
 import Thanks from "./Components/Donations/Thanks";
-import BackofficeUsers from './Components/Backoffice/Users/BackofficeUsers'
+import BackofficeUsers from "./Components/Backoffice/Users/BackofficeUsers";
 function App() {
   return (
     <>
       <Provider store={store}>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/create-activity" component={ActivitiesForm} />
-              <Route path="/create-category" component={CategoriesForm} />
-              <Route path="/create-news" component={NewsForm} />
-              <Route path="/backoffice/members/edit" component={MemberEditCreate} />
-              <Route
-                path="/backoffice/organization/edit"
-                component={OrganizationEditionForm}
-              />
-              <Route path="/backoffice/create-slide" component={SlidesForm} />
-              <Route path="/backoffice/news" component={NewsListEditTable} />
-              <Route path="/create-testimonials" component={TestimonialForm} />
-              <Route path="/create-user" component={UserForm} />
-              <Route path="/create-member" component={MembersForm} />
-              <Route path="/create-project" component={ProjectsForm} />
-              <Route path="/school-campaign" component={SchoolCampaign} />
-              <Route path="/toys-campaign" component={ToysCampaign} />
-              <Route path="/edit-activity/:activityId" component={ActivitiesForm} />
-              <Route
-                path="/edit-testimonial/:testimonialId"
-                component={TestimonialForm}
-              />
-              <Route path="/edit-project/:projectId" component={ProjectsForm} />
-              <Route path="/novedades/:id" component={NewsDetailLayout} />
-              <Route path="/novedades" component={NewsList} />
-              <Route path="/activity-detail/:id" component={Detail} />
-              <Route path="/backoffice" component={Backoffice} />
-              <Route path="/backoffice/members" component={MembersResults} />
-              <Route path="/activities" component={ActivitiesList} />
-              <Route path="/nosotros" component={AboutUs} />
-              <Route path="/create-contact-message" component={ContactForm} />
-              <Route path="/donate" component={Donation} />
-              <Route path="/thanks" component={Thanks} />
-              <Route path="/backoffice/home" component={EditHomeForm} />
-              <Route path='/list-slides' component={ListSlides}/>
-            </Switch>
-          </BrowserRouter>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/create-activity" component={ActivitiesForm} />
+            <Route path="/create-category" component={CategoriesForm} />
+            <Route path="/create-news" component={NewsForm} />
+            <Route
+              path="/backoffice/members/edit"
+              component={MemberEditCreate}
+            />
+            <Route
+              path="/backoffice/organization/edit"
+              component={OrganizationEditionForm}
+            />
+            <Route path="/backoffice/create-slide" component={SlidesForm} />
+            <Route path="/backoffice/news" component={NewsListEditTable} />
+            <Route path="/create-testimonials" component={TestimonialForm} />
+            <Route path="/create-user" component={UserForm} />
+            <Route path="/create-member" component={MembersForm} />
+            <Route path="/create-project" component={ProjectsForm} />
+            <Route path="/school-campaign" component={SchoolCampaign} />
+            <Route path="/toys-campaign" component={ToysCampaign} />
+            <Route
+              path="/edit-activity/:activityId"
+              component={ActivitiesForm}
+            />
+            <Route
+              path="/edit-testimonial/:testimonialId"
+              component={TestimonialForm}
+            />
+            <Route path="/edit-project/:projectId" component={ProjectsForm} />
+            <Route path="/novedades/:id" component={NewsDetailLayout} />
+            <Route path="/novedades" component={NewsList} />
+            <Route path="/activity-detail/:id" component={Detail} />
+            <Route exact path="/backoffice" component={Backoffice} />
+            <Route path="/backoffice/members" component={MembersResults} />
+            <Route path="/activities" component={ActivitiesList} />
+            <Route path="/nosotros" component={AboutUs} />
+            <Route path="/create-contact-message" component={ContactForm} />
+            <Route path="/donate" component={Donation} />
+            <Route path="/thanks" component={Thanks} />
+            <Route path="/backoffice/home" component={EditHomeForm} />
+            <Route path="/list-slides" component={ListSlides} />
+          </Switch>
+        </BrowserRouter>
       </Provider>
     </>
   );
