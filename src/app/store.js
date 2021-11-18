fac276050801a7f54d1205ec8d055089a832eaf6
julian624 <julianperezcv@hotmail.com>
2021-11-18 16:45:15 -0300

    Created slice for members reducer and apply changes in members backoffice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authReducer/authReducer';
+import membersReducer from './MembersReducer/membersReducer';
 
 const store = configureStore({
   reducer: {
     authorization: authReducer,
-  },
+    members: membersReducer,
+   },
 });
 
 export default store
