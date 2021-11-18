586d653da0479255151f6d2e2c5157660fabe501
Martin Caparros <martincaparross@gmail.com>
2021-11-18 11:45:05 -0300

    Already fixed all the functions, change the name of the request GetSlideById, all tested and working correctly.

--- src/Services/slidesServices.js
+++ src/Services/slidesServices.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import Swal from 'sweetalert2';
 
-const getSlides = () => {
-  const response = axios.get(`http://ongapi.alkemy.org/api/slides`);
-  return response;
+const getSlides = async () => {
+  const response = await axios.get(`http://ongapi.alkemy.org/api/slides`);
+  return(response.data.data);
 };
 
 const getImagesSlides = async () => {
   try {
     const res = await getSlides();
-    const dataImage = res.data.data.filter(
+    const dataImage = res.filter(
       (item) => item.image !== null && item.image !== ""
     );
     return dataImage;
@@ -17,55 +17,40 @@ const getImagesSlides = async () => {
     console.log(err);
   }
 };
-const CreateSlide = (data) => {
-  axios({
-    method: "POST",
-    url: "http://ongapi.alkemy.org/api/slides",
-    data: {
-      name: data.name,
-      description: data.description,
-      image: data.image,
-    },
-  })
-  .then((res) => {
-        Swal.fire(
-          'Slide Creado!'
-        )
-    })
-    .catch((err) => (err));
+
+const CreateSlide = async (data) => {
+  try {
+    const response = await axios.post('http://ongapi.alkemy.org/api/slides',data)
+    Swal.fire('Slide Creado!')
+    return response
+  } catch (error) {
+    console.log(error)
+  }
 }
-const EditSlide = () => {
-  axios({
-  method: "PUT",
-  url: `http://ongapi.alkemy.org/api/slides/${slides.id}`,
-  data: {
-    name: data.name,
-    description: data.description,
-    image: data.image,
-  },
-  })
+const EditSlide = async (id,data) => {
+  try{
+    const response = await axios.put(`http://ongapi.alkemy.org/api/slides/${id}`,data)
+    return response;
+  }catch(error){
+    console.log(error)
+  }
 }
 
-const GetAllSlidesById = () => {
-  axios({
-    method: "GET",
-    url: `http://ongapi.alkemy.org/api/slides/${slides.id}`,
-   })
-  .then(res => (res))
-  .catch(err => (err))
+const GetSlidesById = async (id) => {
+  try {
+    const data = await axios.get(`http://ongapi.alkemy.org/api/slides/${id}`)
+    return data
+  }catch (error) {
+    console.log(error)
+  }
 }
-const DeleteSlide = () => {
-  axios({
-    method: "DELETE",
-    url: `http://ongapi.alkemy.org/api/slides/${slides.id}`,
-    data: {
-      name: data.name,
-      description: data.description,
-      image: data.image,
-    },
-   })
-  .then(res => (res))
-  .catch(err => (err))
+const DeleteSlide = async (id) => {
+  try{
+    const slideToDelete = await axios.delete(`http://ongapi.alkemy.org/api/slides/${id}`)
+    return slideToDelete
+  }catch(error) {
+    console.log(error)
+  }
 }
 
-export {CreateSlide, EditSlide, GetAllSlidesById, DeleteSlide, getSlides, getImagesSlides };
+export {CreateSlide, EditSlide, GetSlidesById, DeleteSlide, getSlides, getImagesSlides };
