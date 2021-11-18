4f827824c6d17e403496425bee1d7f7695942e3f
julian624 <julianperezcv@hotmail.com>
2021-11-18 20:23:46 -0300

    Changed some errors and created the loaders for activities and details

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -5,22 +5,32 @@ import {ACTIVITIES} from '../common/text/textActivity';
 import {getAllActivities} from '../../Services/activityService';
 import ActivitiesCards from './ActivitiesCards/ActivitiesCards';
 import ListPagination from '../common/ListPagination/ListPagination';
+import LoadingSpinner from "../../Utils/loadingSpinner";
 const ActivitiesList = () => {
 
     const [items, setItems] = useState([]);
+    const [loading, setIsLoading] = useState(true);
 
     useEffect(async() => {
-        getAllActivities()
-            .then(allActivities => setItems(allActivities));
+         const allActivities = await getAllActivities()
+            setItems(allActivities);
+            setIsLoading(false);
     }, []);
-
     const showItemsListComponent = (items) => (<ActivitiesCards activities={items} />);
-
     return (
         <div>
-            <Title title={ACTIVITIES} id={'activitiesList'}/>
-            <ListPagination items={items} showItemsListComponent={showItemsListComponent } />
-        </div>
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            <Title title={ACTIVITIES} id={"activitiesList"} />
+            <ListPagination
+              items={items}
+              showItemsListComponent={showItemsListComponent}
+            />
+          </>
+        )}
+      </div>
     );
 };
 
