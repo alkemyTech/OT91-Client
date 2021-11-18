25643e6eafae63a22a8b348a31d268d18768329c
andreajhl <andrea.ahernandez@mercadolibre.com>
2021-11-17 23:20:45 -0300

    add component video for last event

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -4,6 +4,7 @@ import { listHasValues } from "../../Utils/validation";
 import Title from "../Title/Title";
 import CustomCard from "../Card/CustomCard";
 import { Container } from "@mui/material";
+import VideoCard from "../Card/VideoCard";
 const lastNewsMock = [
   {
     id: 626,
@@ -29,12 +30,17 @@ const lastNewsMock = [
       "https://www.kleineherzen.or.at/wp-content/uploads/Kids_Mutter_Kind_haus2019.png",
   },
 ];
+const videoLastEvent={
+  name: 'Asistencia a comedores comunitarios en Buenos Aires.',
+  content:'Donación de 42 toneladas de alimentos para 5 comedores comunitarios en el partido de San Martín, Buenos Aires, que diariamente atienden a 1210 personas.',
+  video:'https://youtu.be/4YnSk1gI_Oo'
+};
 const NewsList = ({ lastNews }) => {
-  const newsListHasValues = listHasValues(lastNewsMock);
+  const newsListHasValues = listHasValues([...lastNewsMock,]);
   return (
     <Container className="ContainerList">
       <Title title="Novedades" />
-      <ul className="list-container">
+      <ul className="list-container-novedades">
         {newsListHasValues ? (
           lastNewsMock.map((news) => {
             return (
@@ -49,6 +55,11 @@ const NewsList = ({ lastNews }) => {
         ) : (
           <p>No hay novedades</p>
         )}
+      <VideoCard
+        title={videoLastEvent.name}
+        video={videoLastEvent.video}
+        description={videoLastEvent.content}
+      />
       </ul>
     </Container>
   );
