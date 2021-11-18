d1225eb221ff4cbadecfcb5a8e36e3150a3f007a
ArielSurco <arielsurcoa@gmail.com>
2021-11-18 17:52:59 -0300

    Add createActivity and updateActivity thunks

--- src/app/activitiesReducer/activitiesReducer.js
+++ src/app/activitiesReducer/activitiesReducer.js
@@ -1,25 +1,12 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import * as activityService from '../../Services/activityService';
-import axios from 'axios';
 
-const URL = process.env.REACT_APP_ACTIVITY_URL
+export const getAllActivities = createAsyncThunk("activities/getAllActivities", activityService.getAllActivities);
+export const getActivity = createAsyncThunk('activities/getActivity', activityService.getActivityById);
+export const createActivity = createAsyncThunk('activities/createActivity', activityService.createActivity);
+export const updateActivity = createAsyncThunk('activities/updateActivity', activityService.modifyActivity);
+export const deleteActivity = createAsyncThunk('activities/deleteActivity', activityService.deleteActivityById);
 
-export const getAllActivities = createAsyncThunk(
-    "activities/getAllActivities",
-    activityService.getAllActivities
-)
-export const getActivity = createAsyncThunk(
-    'activities/getActivity',
-    activityService.getActivityById
-)
-export const createOrUpdateActivity = createAsyncThunk(
-    'activities/createOrUpdateActivity',
-    activityService.createOrUpdateActivity
-)
-export const deleteActivity = createAsyncThunk(
-    'activities/deleteActivity',
-    activityService.deleteActivityById()
-)
 const activitiesSlice = createSlice({
     name:"activitiesReducer",
     initialState: {
@@ -28,27 +15,30 @@ const activitiesSlice = createSlice({
         status: ''
     },
     extraReducers: {
-        [getAllActivities.pending]: (state) => state.status = 'loading',
+        [getAllActivities.pending]: (state) => { state.status = 'loading' },
         [getAllActivities.fulfilled]: (state, action) => {
             state.status = 'success';
             state.activities = action.payload;
         },
-        [getAllActivities.rejected]: (state) => state.status = 'failed',
-        [getActivity.pending]: (state) => state.status = 'loading',
+        [getAllActivities.rejected]: (state) => { state.status = 'failed' },
+        [getActivity.pending]: (state) => { state.status = 'loading' },
         [getActivity.fulfilled]: (state, action) => {
             state.status = 'success';
-            state.activity = action.payload;
+            state.activity = action.payload.data;
         },
-        [getActivity.rejected]: (state) => state.status = 'failed',
-        [createOrUpdateActivity.pending]: (state) => state.status = 'loading',
+        [getActivity.rejected]: (state) => { state.status = 'failed' },
+        [createOrUpdateActivity.pending]: (state) => { state.status = 'loading' },
         [createOrUpdateActivity.fulfilled]: (state, action) => {
             state.status = 'sucess';
             state.activity = action.payload;
         },
-        [createOrUpdateActivity.rejected]: (state) => state.status = 'failed',
-        [deleteActivity.pending]: (state) => state.status = 'loading',
-        [deleteActivity.fulfilled]: (state) => state.status = 'success',
-        [deleteActivity.rejected]: (state) => state.status = 'failed'
+        [createOrUpdateActivity.rejected]: (state) => { state.status = 'failed' },
+        [deleteActivity.pending]: (state) => { state.status = 'loading' },
+        [deleteActivity.fulfilled]: (state, action) => {
+            state.status = 'success';
+            state.activities = state.activities.filter(deletedActivity => deletedActivity.id != action.meta.arg);
+        },
+        [deleteActivity.rejected]: (state) => { state.status = 'failed' }
     }
 });
 
