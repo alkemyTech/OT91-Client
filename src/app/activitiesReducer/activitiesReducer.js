61206477459af9aa4c760d2d1f3980398fd35e83
ArielSurco <arielsurcoa@gmail.com>
2021-11-18 18:27:32 -0300

    Add createOrUpdateActivity thunk

--- src/app/activitiesReducer/activitiesReducer.js
+++ src/app/activitiesReducer/activitiesReducer.js
@@ -6,6 +6,7 @@ export const getActivity = createAsyncThunk('activities/getActivity', activitySe
 export const createActivity = createAsyncThunk('activities/createActivity', activityService.createActivity);
 export const updateActivity = createAsyncThunk('activities/updateActivity', activityService.modifyActivity);
 export const deleteActivity = createAsyncThunk('activities/deleteActivity', activityService.deleteActivityById);
+export const createOrUpdateActivity = createAsyncThunk('activities/createOrUpdateActivity', activityService.createOrUpdateActivity);
 
 const activitiesSlice = createSlice({
     name:"activitiesReducer",
@@ -27,18 +28,31 @@ const activitiesSlice = createSlice({
             state.activity = action.payload.data;
         },
         [getActivity.rejected]: (state) => { state.status = 'failed' },
-        [createOrUpdateActivity.pending]: (state) => { state.status = 'loading' },
-        [createOrUpdateActivity.fulfilled]: (state, action) => {
-            state.status = 'sucess';
-            state.activity = action.payload;
-        },
-        [createOrUpdateActivity.rejected]: (state) => { state.status = 'failed' },
         [deleteActivity.pending]: (state) => { state.status = 'loading' },
         [deleteActivity.fulfilled]: (state, action) => {
             state.status = 'success';
             state.activities = state.activities.filter(deletedActivity => deletedActivity.id != action.meta.arg);
         },
-        [deleteActivity.rejected]: (state) => { state.status = 'failed' }
+        [deleteActivity.rejected]: (state) => { state.status = 'failed' },
+        [createActivity.pending]: (state) => { state.status = 'loading' },
+        [createActivity.fulfilled]: (state, action) => {
+            state.status = 'success';
+            state.activities = [...state.activities, action.payload];
+        },
+        [createActivity.rejected]: (state) => { state.status = 'failed' },
+        [updateActivity.pending]: (state) => { state.status = 'loading' },
+        [updateActivity.fulfilled]: (state, action) => {
+            state.status = 'success';
+            const updatedActivityIndex = state.activities.findIndex(activity => activity.id == action.payload.id);
+            state.activities[updatedActivityIndex] = action.payload;
+        },
+        [updateActivity.rejected]: (state) => { state.status = 'failed' },
+        [createOrUpdateActivity.pending]: (state) => { state.status = 'loading'},
+        [createOrUpdateActivity.fulfilled]: (state, action) => {
+            state.status = 'success';
+            getAllActivities(action.payload.id);
+        },
+        [createOrUpdateActivity.rejected]: (state) => { state.status = 'failed' }
     }
 });
 
