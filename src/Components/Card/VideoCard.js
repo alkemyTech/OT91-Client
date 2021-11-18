1464b141c60600959a033ff41de343d3da18717b
andreajhl <andrea.ahernandez@mercadolibre.com>
2021-11-17 23:21:40 -0300

    add card component for videos, with description and title

---
+++ src/Components/Card/VideoCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import {Card,CardContent,Typography} from '@mui/material';
+import VideoPlayer from '../common/VideoPlayer';
+
+const VideoCard = ({title,video,description}) => {
+
+    return (
+        <Container sx={{ display: 'inline-flex' }}>
+            <Card sx={{ maxWidth: 345 }}>
+                <div>
+                    <VideoPlayer video={video} />
+                </div>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title || ""}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {description || ""}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Container>
+    );
+}
+
+export default VideoCard;
