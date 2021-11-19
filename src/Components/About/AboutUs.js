0ad17934e60fda86151f7f9bc15639561b38366a
Telma <telmakn@gmail.com>
2021-11-19 10:29:17 -0300

    Add social media components

--- src/Components/About/AboutUs.js
+++ src/Components/About/AboutUs.js
@@ -1,7 +1,11 @@
 import Title from "../Title/Title";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import "../../Styles/Container.css";
-import AboutUsMembers from "./AboutUsMembers";
+import {
+  LinkedinCompanyProfile,
+  LinkedinFollowCompany,
+  TwitterTweet,
+} from "react-social-plugins";
 
 const AboutUs = () => {
   const description = `Somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de:
@@ -12,6 +16,35 @@ const AboutUs = () => {
         <Title title="Nosotros" />
         <div className="title-container-text">
           <p>{description}</p>
+          <div>
+            <LinkedinCompanyProfile
+              lang="en_US"
+              companyId={68737437}
+              format="inline"
+              text="Company name"
+            />
+            <TwitterTweet
+              align="center"
+              coversation="none"
+              tweetId="1450535690199085058"
+              theme="light"
+              width={300}
+            />
+            <TwitterTweet
+              align="center"
+              coversation="none"
+              tweetId="1440383594615042052"
+              theme="light"
+              width={300}
+            />
+            <TwitterTweet
+              align="center"
+              coversation="none"
+              tweetId="1271501359658012675"
+              theme="light"
+              width={300}
+            />
+          </div>
         </div>
       </Box>
       <AboutUsMembers />
