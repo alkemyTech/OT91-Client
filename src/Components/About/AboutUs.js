6bc6bb77086698cdb86c86906647a581a0fb00c6
andreajhl <andrea.ahernandez@mercadolibre.com>
2021-11-20 02:57:11 -0300

    add spinner loading in home and about

--- src/Components/About/AboutUs.js
+++ src/Components/About/AboutUs.js
@@ -1,18 +1,25 @@
 import Title from "../Title/Title";
 import { Box } from "@mui/material";
 import '../../Styles/Container.css';
+import LoadingSpinner from "../../Utils/loadingSpinner";
 
 const AboutUs = () => {
     const description = `Somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de:
         Educación, deportes, primera infancia, salud, alimentación y trabajo social.`
     return (
         <>
-            <Box className="title-container">
-                <Title title="Nosotros"/>
-                <div className="title-container-text">
-                    <p>{description}</p>
-                </div>
-            </Box>
+            {description ? <Box className="title-container">
+                    <Title title="Nosotros"/>
+                    <div className="title-container-text">
+                        <p>{description}</p>
+                    </div>
+                </Box>: <LoadingSpinner
+                    type="ThreeDots"
+                    color="#000000"
+                    height={50}
+                    width={100}
+                />
+            }
         </>
     )
 }
