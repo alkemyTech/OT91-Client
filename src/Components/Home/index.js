90597a70f49c6dd2146db0e9c86bd44c5fdb164f
andreajhl <andrea.ahernandez@mercadolibre.com>
2021-11-20 02:56:37 -0300

    add spinner loading in home and about

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Carousel from "../Carousel/Carousel";
+import LoadingSpinner from "../../Utils/loadingSpinner";
 
-const Home = () => {
+const Home = ({listNews}) => {
+
   return (
     <div>
-      <h2>Titulo de bienvenida</h2>
-      <section>
-        <Carousel />
-        <div>.map() del listado general de noticias</div>
-      </section>
+     {listNews ? <div>
+          <h2>Titulo de bienvenida</h2>
+          <section>
+              <Carousel />
+              <div>.map() del listado general de noticias</div>
+          </section>
+       </div> :<LoadingSpinner
+          type="ThreeDots"
+          color="#000000"
+          height={50}
+          width={100}
+        />
+      }
     </div>
   );
 };
