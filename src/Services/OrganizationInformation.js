de03ebb27c8ab27a4f8954bfc6de0e0c7e73062c
andreajhl <andrea.ahernandez@mercadolibre.com>
2021-11-20 03:00:01 -0300

    agg functions with try catch to return error if they fail, they are only mock-ups

--- src/Services/OrganizationInformation.js
+++ src/Services/OrganizationInformation.js
@@ -1,6 +1,50 @@
 import axios from 'axios';
+import { AlertError } from '../Components/common/alerts/Alerts';
 
-export const getOrganizationInformation = async () => {
-    const {data} = await axios.get('http://ongapi.alkemy.org/api/organization')
-    return data
-}
+export const getOrganizationInformation = () => {
+    try {
+      export const {data} = axios.get(`http://ongapi.alkemy.org/api/organization`);
+      return data;
+    } catch ({response}) {
+      AlertError(response.status,response.data.message);
+      return {success:false};
+    }
+};
+  
+export const getByIOrganizationInformation = () => {
+    try {
+  
+    } catch ({response}) {
+      AlertError(response.status,response.data.message);
+      return {success:false};
+    };
+};
+  
+export const updateOrganizationInformation  = () => {
+    try {
+  
+    } catch ({response}) {
+      AlertError(response.status,response.data.message);
+      return {success:false};
+    };
+};
+  
+  
+export const createOrganizationInformation  = () => {
+    try {
+  
+    } catch ({response}) {
+      AlertError(response.status,response.data.message);
+      return {success:false};
+    };
+};
+
+export const deleteOrganizationInformation  = () => {
+    try {
+  
+    } catch ({response}) {
+      AlertError(response.status,response.data.message);
+      return {success:false};
+    };
+};
+  
--- src/Services/slidesService.js
+++ src/Services/slidesService.js
@@ -2,9 +2,51 @@ import axios from "axios";
 import { AlertError } from '../Components/common/alerts/Alerts';
 
 const getSlides = () => {
-  const response = axios.get(`http://ongapi.alkemy.org/api/slides`);
-  return response;
+  try {
+    const response = axios.get(`http://ongapi.alkemy.org/api/slides`);
+    return response.data;
+  } catch ({response}) {
+    AlertError(response.status,response.data.message);
+    return {success:false};
+  }
 };
+
+const getByIdSlides = (id) => {
+  try {
+
+  } catch ({response}) {
+    AlertError(response.status,response.data.message);
+    return {success:false};
+  };
+};
+
+const updateSlides = (id) => {
+  try {
+
+  } catch ({response}) {
+    AlertError(response.status,response.data.message);
+    return {success:false};
+  };
+};
+
+
+const createSlides = (body) => {
+  try {
+
+  } catch ({response}) {
+    AlertError(response.status,response.data.message);
+    return {success:false};
+  };
+};
+const deleteSlider = (id) => {
+  try {
+
+  } catch ({response}) {
+    AlertError(response.status,response.data.message);
+    return {success:false};
+  };
+};
+
 
 const getImagesSlides = async () => {
   try {
@@ -13,8 +55,8 @@ const getImagesSlides = async () => {
       (item) => item.image !== null && item.image !== ""
     );
     return dataImage;
-  } catch (error) {
-    AlertError(error.response.status,error.response.data.message);
+  } catch ({response}) {
+    AlertError(response.status,response.data.message);
     return {success:false};
   }
 };
