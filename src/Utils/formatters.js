b48deef45d20ea8d9afc9c2aa4f362d1161d4529
Telma <telmakn@gmail.com>
2021-11-23 16:49:42 -0300

    url format

--- src/Utils/formatters.js
+++ src/Utils/formatters.js
@@ -1,10 +1,10 @@
 export const formatDate = (date) => date.toLocaleDateString("en-GB");
-import { useEffect } from "react";
+
 export const URLFileFormater = (e, state, setState, inputName) => {
   const imageFile = e.target.files[0];
-  const reader = new FileReader();
-  reader.readAsDataURL(imageFile);
-  reader.onload = (e) => {
+  const imageUrl = new FileReader();
+  imageUrl.readAsDataURL(imageFile);
+  imageUrl.onload = (e) => {
     setState({ ...state, [inputName]: e.target.result });
   };
 };
