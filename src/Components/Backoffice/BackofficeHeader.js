ac1a33b7d0dd66996530514a165f0f4dc52d2c07
Martin Caparros <martincaparross@gmail.com>
2021-11-23 13:06:56 -0300

    Side bar created for backoffice navigation, it opens onClick on menu icon and close on leftArrow icon onClick.

---
+++ src/Components/Backoffice/BackofficeHeader.js
@@ -0,0 +1,92 @@
+import React, {useState} from 'react';
+import {styled,useTheme} from '@mui/material/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronRight from '@mui/icons-material/ChevronRight';
+import ChevronLeft from '@mui/icons-material/ChevronLeft';
+import {IconButton,
+        Box,
+        Typography,
+        Drawer,
+} from '@mui/material';
+import MuiAppBar from '@mui/material/AppBar';
+
+const BackofficeHeader = () => {
+
+  const drawerWidth = 240;
+
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+    })(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
+  const theme = useTheme();
+
+  const [open,setOpen] = useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+
+  return (
+    <>
+      <AppBar position="static" sx={{heigth:20,display:'flex',flexDirection:'row',alignItems:'center'}}>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={{ ml: 2,mr:2}}
+        >
+          <MenuIcon/>
+        </IconButton>
+        <Typography variant="h6" component="div">
+          Somos Mas
+        </Typography>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeft /> : <ChevronRight />}
+          </IconButton>
+        </DrawerHeader>
+      </Drawer>
+    </>
+  );
+};
+
+export default BackofficeHeader;
