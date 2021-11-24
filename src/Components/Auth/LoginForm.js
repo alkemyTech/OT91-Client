e6fa1e579c4c750cebc7aafeb20015b18b8df526
Martin Caparros <martincaparross@gmail.com>
2021-11-24 11:14:22 -0300

    Found this problem creating another component. React and useState import missing.

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import { hasRequiredError, regExp, validValue } from '../../Utils/validation';
 import '../FormStyles.css';
