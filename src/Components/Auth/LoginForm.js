e58482159fad3bc42532b7423a51f2c7ef7a42f9
Martin Caparros <martincaparross@gmail.com>
2021-11-24 11:14:53 -0300

    console.log() deleted

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -18,7 +18,6 @@ const LoginForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(initialValues);
         localStorage.setItem('token', 'tokenValueExample')
     }
 
