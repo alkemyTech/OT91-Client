e8868d1567ca695c587b1cd896f35aa458038764
ArielSurco <arielsurcoa@gmail.com>
2021-11-25 20:11:59 -0300

    Add Countdown to Content component

--- src/Campaigns/School/Content.js
+++ src/Campaigns/School/Content.js
@@ -1,28 +1,44 @@
 import { Container, Box, ImageList, Typography } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Image from '../../Assets/SchoolCampaign/Logotipo campaña materiales escolares.png';
 import Image2 from '../../Assets/SchoolCampaign/Imágenes contenido opción 1.png';
 import Image3 from '../../Assets/SchoolCampaign/Imágenes contenido opción 2.png';
+import { formatDate } from '../../Utils/formatters';
 const Content = () => {
+  const campaignDate = new Date(2021,11,15,15,30);
+  const currentDate = new Date();
+  const calculateRemainingTime = () => {
+    let seconds = Math.floor((campaignDate - (currentDate))/1000);
+    let minutes = Math.floor(seconds/60);
+    let hours = Math.floor(minutes/60);
+    const days = Math.floor(hours/24);
+    hours = hours-(days*24);
+    minutes = minutes-(days*24*60)-(hours*60);
+    seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+    return [days, hours, minutes, seconds];
+  }
+  const timeLeft = calculateRemainingTime()
+  const timeLeftFormatted = `${timeLeft[0]}d ${timeLeft[1]}h ${timeLeft[2]}m`;
+
+  const images = [
+    {img: Image, alt:"", rotate:'45deg', align:'start'},
+    {img: Image2, alt:"", rotate:'10deg', align:'end'},
+    {img: Image3, alt:"", rotate:'-10deg', align:'end'},
+    {img: Image, alt:"", rotate:'-45deg', align:'start'}
+  ]
   return (
     <Container width={false} sx={{display:'flex', alignItems:'center', flexDirection:'column', position:'relative', marginTop:'100px'}}>
-      <Box sx={{display: 'flex', flexDirection: 'column', alignItems:'center', textAlign:'center', zIndex:'10'}}>
+      <Box sx={{display: 'flex', flexDirection: 'column', alignItems:'center', textAlign:'center', zIndex:'10', gap:'15px'}}>
         <Box sx={{display:'flex', flexDirection:{xs:'column', md:'row'}, justifyContent:'center', maxWidth:{lg:'70%'}, flexWrap:'wrap', gap:'10px'}}>
-          <Typography variant="h5">01-01-1900 13:15 hs </Typography>
+          <Typography variant="h5">{campaignDate.toLocaleString('en-GB').slice(0, -3)}hs</Typography>
           <Typography variant="h5">Avenida Lacarra 621, Parque avellaneda, Provincia de buenos aires</Typography>
         </Box>
-        <Typography variant="h5" >TE QUEDAN: 8d 3h 23m para participar</Typography>
+        <Typography variant="h5" sx={{display: {xs:'none', sm:'block'}}}>TE QUEDAN: {timeLeftFormatted} para participar</Typography>
         <Typography paragraph sx={{maxWidth:{xs:'100%', lg:'65%'}}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam atque harum vero, in nobis
          suscipit ullam reprehenderit debitis fugiat, neque, aliquam est ipsam maiores repellendus cum voluptate at tenetur perferendis.</Typography>
-        <Typography variant="h5" sx={{display: {xs:'none', sm:'block'}}}>
-          Countdown
-        </Typography>
       </Box>
-      <Box sx={{display:{xs:'none', lg:'flex'}, minHeight:'300px', justifyContent:'space-between', position:'absolute', width:'100%', top:'50px'}}>
-        <img src={Image} alt="" width="200px" style={{transform:'rotate(45deg)', height:'fit-content'}}/>
-        <img src={Image2} width="200px" style={{transform:'rotate(10deg)', height:'fit-content', alignSelf:'end'}}/>
-        <img src={Image3} width="200px"  style={{transform:'rotate(-10deg)', height:'fit-content', alignSelf:'end'}}/>
-        <img src={Image} width="200px" style={{transform:'rotate(-45deg)', height:'fit-content'}} />
+      <Box sx={{display:{xs:'none', lg:'flex'}, minHeight:'300px', justifyContent:'space-between', position:'absolute', width:'100%', top:'60px'}}>
+        {images.map((image) => <img src={image.img} alt={image.alt} width="200px" style={{transform:`rotate(${image.rotate})`, height:'fit-content', alignSelf:image.align}}/> )}
       </Box>
     </Container>
   );
