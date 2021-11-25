11083b88b9dec797492136de04fb18d5b8db0381
Martin Caparros <martincaparross@gmail.com>
2021-11-25 19:02:23 -0300

    I create 3 new components for better modularization and have a more readeable code, also created a theme to unify some css, it can be also used as theme provider in App.js but its not on this ticket scope to create a theme provider to all the aplication so i only created for styling the header.

--- src/Components/Backoffice/BackofficeHeader.js
+++ src/Components/Backoffice/BackofficeHeader.js
@@ -1,45 +1,31 @@
-import React, {useState} from 'react';
-import {styled,useTheme} from '@mui/material/styles';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronRight from '@mui/icons-material/ChevronRight';
-import ChevronLeft from '@mui/icons-material/ChevronLeft';
-import {IconButton,
-        Box,
-        Typography,
-        Drawer,
+import React, { useState } from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import {
+  Box,
+  Drawer,
+  CssBaseline,
+  Toolbar,
+  List,
+  Typography,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from '@mui/material';
-import MuiAppBar from '@mui/material/AppBar';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Main } from '../../Utils/MUI Components/Main';
+import { AppBar } from '../../Utils/MUI Components/AppBar';
+import { DrawerHeader } from '../../Utils/MUI Components/DrawerHeader';
+import { tema } from '../../Styles/Theme/GlobalTheme';
 
 const BackofficeHeader = () => {
+  const [open, setOpen] = useState(false);
 
-  const drawerWidth = 240;
-
-  const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  }));
-  const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-    })(({ theme, open }) => ({
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: `${drawerWidth}px`,
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
-  }));
   const theme = useTheme();
-
-  const [open,setOpen] = useState(false)
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -48,44 +34,54 @@ const BackofficeHeader = () => {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-
-
   return (
-    <>
-      <AppBar position="static" sx={{heigth:20,display:'flex',flexDirection:'row',alignItems:'center'}}>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawerOpen}
-          edge="start"
-          sx={{ ml: 2,mr:2}}
+    <ThemeProvider theme={tema}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar color="boton" position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="white" noWrap component="div">
+              Somos mas
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          sx={{
+            width: 240,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: 240,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
         >
-          <MenuIcon/>
-        </IconButton>
-        <Typography variant="h6" component="div">
-          Somos Mas
-        </Typography>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-          width: drawerWidth,
-          boxSizing: 'border-box',
-        },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeft /> : <ChevronRight />}
-          </IconButton>
-        </DrawerHeader>
-      </Drawer>
-    </>
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+            <List sx={{ ml:1}}>
+            </List>
+          <Divider/>
+        </Drawer>
+        <Main open={open}>
+          <DrawerHeader />
+        </Main>
+      </Box>
+    </ThemeProvider>
   );
 };
 
