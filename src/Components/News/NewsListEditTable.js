44042b1e40b547453c26bac196a2c6c2216493fe
andreajhl <andrea.ahernandez@mercadolibre.com>
2021-11-26 11:18:46 -0300

    add componente search in news

--- src/Components/News/NewsListEditTable.js
+++ src/Components/News/NewsListEditTable.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import {
   TableContainer,
   Table,
@@ -11,10 +11,45 @@ import {
 import { Link } from "react-router-dom";
 import { NewsTableRows } from "./NewsTableRows";
 import { createNewsObject } from "../../Services/newsServices";
+import { getSearch} from '../../app/NewsReducer/newsReducer'
+import { useSelector, useDispatch} from 'react-redux';
+import { debounceHook } from "../../Utils/debounce";
+
+const newState = ({news})=> news.newsSearch;
 
 const NewsListEditTable = () => {
+  const dispatch = useDispatch();
+  const news = useSelector(newState);
+  const [search, setSearch] = useState('');
+  const [newsList, setNewsList] = useState([])
+
+  const handleInput = ({target}) => setSearch(target.value);
+
+  const debounceFecthData = useMemo( () => {
+    dispatch(getSearch(search))
+    return debounceHook(dispatch,getSearch,300);
+  },[]);
+
+  useEffect(() => {
+    search !== '' && debounceFecthData(search);
+  },[search, debounceFecthData]);
+
+  useEffect(() => {
+    search !== ''? setNewsList(news) : setNewsList([])
+  }, [news])
+
   return (
     <>
+    <div>
+      <div>
+        <input type='text' value={search} onChange={handleInput} />
+        <div>
+          <ul>
+            {newsList.map(e=><li key={e.id} onClick={()=>setSearch(e.name)}>{e.name}</li>)}
+          </ul>
+        </div>
+      </div>
+    </div>
       <TableContainer>
         <Table size="small" aria-label="simple table">
           <TableHead>
