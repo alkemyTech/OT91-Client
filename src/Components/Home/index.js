f7c16f52d5e98ff3f6a7da77efe60b5303bdd303
MarCortina <marmica24@hotmail.com>
2021-11-27 10:51:40 -0300

    changes in carousel home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,23 +2,37 @@ import React, { useEffect, useState } from "react";
 import Carousel from "../Carousel/Carousel";
 import Footer from "../Footer/Footer";
 import { getOrganizationInformation } from "../../Services/OrganizationInformation";
-import Header from '../Layout/Header/Header'
-import CardsSection from './CardsSection';
-import * as newsService from '../../Services/newsServices'
-import * as testimonialService from '../../Services/testimonialService'
+import Header from "../Layout/Header/Header";
+import CardsSection from "./CardsSection";
+import * as newsService from "../../Services/newsServices";
+import * as testimonialService from "../../Services/testimonialService";
 const Home = () => {
   const [welcomeText, setWelcomeText] = useState("");
   useEffect(() => {
-    getOrganizationInformation().then(res => setWelcomeText(res.data.welcome_text))
-  }, [])
+    getOrganizationInformation().then((res) =>
+      setWelcomeText(res.data.welcome_text)
+    );
+  }, []);
   return (
     <div>
-      <Header/>
-      <h2 style={{textAlign:"center"}}>{welcomeText}</h2>
-      <section>
+      <Header />
+      <h2 style={{ textAlign: "center" }}>{welcomeText}</h2>
+      <section
+        style={{
+          maxWidth: "1400px",
+          margin: "0 auto",
+        }}
+      >
         <Carousel />
-        <CardsSection title="Últimas novedades" getInformation={newsService.getAll} button={{text:'Ver todas', to:'/novedades'}}/>
-        <CardsSection title="Testimonios" getInformation={testimonialService.getAllTestimonial}/>
+        <CardsSection
+          title="Últimas novedades"
+          getInformation={newsService.getAll}
+          button={{ text: "Ver todas", to: "/novedades" }}
+        />
+        <CardsSection
+          title="Testimonios"
+          getInformation={testimonialService.getAllTestimonial}
+        />
       </section>
     </div>
   );
