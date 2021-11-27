375390a632c343574e2423d99300c452348e5dd2
andreajhl <andrea.ahernandez@mercadolibre.com>
2021-11-27 19:38:27 -0300

    add alert error in service

--- src/Services/OrganizationInformation.js
+++ src/Services/OrganizationInformation.js
@@ -3,36 +3,34 @@ import { AlertError } from '../Components/common/alerts/Alerts';
 
 export const getOrganizationInformation = () => {
     try {
-      export const {data} = axios.get(`http://ongapi.alkemy.org/api/organization`);
+      const {data} = axios.get(`http://ongapi.alkemy.org/api/organization`);
       return data;
     } catch ({response}) {
       AlertError(response.status,response.data.message);
       return {success:false};
     }
 };
-  
+
 export const getByIOrganizationInformation = () => {
     try {
-  
     } catch ({response}) {
       AlertError(response.status,response.data.message);
       return {success:false};
     };
 };
-  
+
 export const updateOrganizationInformation  = () => {
     try {
-  
+
     } catch ({response}) {
       AlertError(response.status,response.data.message);
       return {success:false};
     };
 };
-  
-  
+
 export const createOrganizationInformation  = () => {
     try {
-  
+
     } catch ({response}) {
       AlertError(response.status,response.data.message);
       return {success:false};
@@ -41,10 +39,9 @@ export const createOrganizationInformation  = () => {
 
 export const deleteOrganizationInformation  = () => {
     try {
-  
+
     } catch ({response}) {
       AlertError(response.status,response.data.message);
       return {success:false};
     };
 };
-  
