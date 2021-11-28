b7680e0f02c88224a29f794ae5ae7b89d45527be
MarCortina <marmica24@hotmail.com>
2021-11-28 16:38:19 -0300

    add changes to contact form

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,45 +1,54 @@
-import React, { useState } from 'react'
-import { useFormik } from 'formik'
-import * as validations from '../../Services/validations'
-import '../FormStyles.css'
-import GenericInput from '../inputsForms/GenericInput'
-import {createContact} from '../../Services/contactSerive'
+import React, { useState, useEffect } from "react";
+import { useFormik } from "formik";
+import * as validations from "../../Services/validations";
+import "../FormStyles.css";
+import GenericInput from "../inputsForms/GenericInput";
+import { createContact } from "../../Services/contactSerive";
+import { getOrganizationInformation } from "../../Services/OrganizationInformation";
+import Map from "../Map/Map";
 
 const validate = (values) => {
-  const errors = {}
-  const inValidEmail = validations.isEmailInvalid(values.email)
-  const phoneInvalid = validations.isShort(values.phone, 8)
+  const errors = {};
+  const inValidEmail = validations.isEmailInvalid(values.email);
+  const phoneInvalid = validations.isShort(values.phone, 8);
   if (inValidEmail) {
-    errors.email = 'Email invalido'
+    errors.email = "Email invalido";
   }
   if (phoneInvalid) {
-    errors.phone = 'Numero de teléfono invalido'
+    errors.phone = "Numero de teléfono invalido";
   }
-  return errors
-}
+  return errors;
+};
 
 const ContactForm = () => {
+  const [coordinates, setCoordinates] = useState([]);
   const inputValues = [
-    { type: 'text', english: 'name', spanish: 'Nombre' },
-    { type: 'email', english: 'email', spanish: 'Email' },
-    { type: 'number', english: 'phone', spanish: 'Teléfono' },
-    { type: 'text', english: 'message', spanish: 'Mensaje' },
-  ]
+    { type: "text", english: "name", spanish: "Nombre" },
+    { type: "email", english: "email", spanish: "Email" },
+    { type: "number", english: "phone", spanish: "Teléfono" },
+    { type: "text", english: "message", spanish: "Mensaje" },
+  ];
 
   const formInitialValues = {
-    name: '',
-    email: '',
-    phone: '',
-    message: '',
-  }
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
 
   const formik = useFormik({
     initialValues: formInitialValues,
     validate,
     onSubmit: (values) => createContact(values),
-  })
+  });
+
+  useEffect(() => {
+    getOrganizationInformation().then((response) => {
+      setCoordinates(JSON.parse(response.data.address));
+    });
+  }, []);
   return (
-    <form className='form-container' onSubmit={formik.handleSubmit}>
+    <form className="form-container" onSubmit={formik.handleSubmit}>
       {inputValues.map((inputValues) => (
         <GenericInput
           name={inputValues.english}
@@ -53,12 +62,12 @@ const ContactForm = () => {
           isTouched={formik.touched[inputValues.english]}
         />
       ))}
-
-      <button className='submit-btn' type='submit'>
+      <Map coordinates={coordinates} />
+      <button className="submit-btn" type="submit">
         Enviar
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
