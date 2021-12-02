2814457b91d139129a15a4a8f2882f14f9dfe368
Andrés Pereyra <andrespereyra.it@gmail.com>
2021-12-02 11:36:06 -0300

    search bar for members in backoffice

--- src/Components/Backoffice/Members/MembersResults.js
+++ src/Components/Backoffice/Members/MembersResults.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Table,
   TableBody,
@@ -14,23 +14,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAll } from "../../../app/MembersReducer/membersReducer";
 import TitleBackoffice from "../../Backoffice/TitleBackoffice";
 import { useHistory } from "react-router-dom";
+import MembersResultsSearchedItem from "./MembersResultsSearchedItems";
+import { TextField, Spinner, Box } from "@mui/material";
+
 const MembersResults = () => {
+  const [isSearching, setIsSearching] = useState(false);
+  const [targetValue, setTargetValue] = useState("");
   const dispatch = useDispatch();
   const members = useSelector((state) => state.members.data);
   const showMembers = () =>
-    members.map((member) => (
-      <MembersResultsItem
-        item={member}
-      />
-    ));
-
+    members.map((member) => <MembersResultsItem item={member} />);
+  const targetValueSearch = (e) => {
+    setTargetValue(e.target.value);
+    setIsSearching(true);
+  };
   useEffect(() => {
     dispatch(getAll());
-  }, []);
+    targetValue.length == 0 && setIsSearching(false);
+  }, [targetValue]);
 
   return (
     <>
       <TitleBackoffice title={"Edición de Miembros"} />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          m: 4,
+        }}
+      >
+        <TextField
+          sx={{ width: "75%" }}
+          controlId="floatingInput"
+          label="Ingrese su busqueda"
+          className="d-flex"
+          onChange={targetValueSearch}
+        />
+      </Box>
+
       <TableContainer className="TableContainer">
         <Table className="TableFinal">
           <TableHead className="TableRowModify">
@@ -57,7 +78,16 @@ const MembersResults = () => {
               </TableCell>
             </TableRow>
           </TableHead>
-          <TableBody color="tablebackground">{showMembers()}</TableBody>
+          {isSearching ? (
+            <MembersResultsSearchedItem
+              endpointName="members"
+              minLength="2"
+              targetValue={targetValue}
+              setIsSearching={setIsSearching}
+            />
+          ) : (
+            <TableBody color="tablebackground">{showMembers()}</TableBody>
+          )}
         </Table>
       </TableContainer>
     </>
