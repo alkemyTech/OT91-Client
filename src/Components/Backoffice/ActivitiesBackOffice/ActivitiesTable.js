d54de9688158a303a480496c17475867ea131e8e
ArielSurco <arielsurcoa@gmail.com>
2021-12-03 00:52:30 -0300

    Use searchInput in ActivitiesTable

--- src/Components/Backoffice/ActivitiesBackOffice/ActivitiesTable.js
+++ src/Components/Backoffice/ActivitiesBackOffice/ActivitiesTable.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { showSuccessAlert } from "../../../Utils/alerts";
 import "../../../Styles/TableStyle.css";
-import { getAll, deleteById } from '../../../app/activitiesReducer/activitiesReducer';
+import { search, deleteById } from '../../../app/activitiesReducer/activitiesReducer';
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import {NewsTableRows} from '../../News/NewsTableRows';
@@ -10,6 +10,8 @@ import {Table,TableBody,TableCell,TableContainer,TableHead,Button,TableRow,IconB
 import { Edit, Delete } from "@mui/icons-material";
 import TitleBackoffice from '../TitleBackoffice'
 import { formatDate } from "../../../Utils/formatters";
+import SearchInput from './SearchInput';
+
 const ActivitiesTable = () => {
   const history = useHistory();
   const { activities } = useSelector(state => state.activities);
@@ -20,65 +22,60 @@ const ActivitiesTable = () => {
     dispatch(deleteById(id));
     showSuccessAlert("Delete Activity");
   };
-
-  useEffect(() => {
-    dispatch(getAll());
-  }, [])
+  const searchActivities = (searchedValue, minLength) => dispatch(search({searchedValue, minLength}))
 
   return (
     <>
      <TitleBackoffice title={"Edición de Actividades"} />
+      <SearchInput searchAction={searchActivities} minLength={3}/>
       <TableContainer className="TableContainer">
-      <Table className="TableFinal">
-        <TableHead className="TableRowModify">
-          <TableRow>
-            <TableCell className="TableCell"align="center">Name</TableCell>
-            <TableCell className="TableCell"align="center">Image</TableCell>
-            <TableCell className="TableCell"align="center">CreatedAt</TableCell>
-            <TableCell align="center" className="TableCell">
-                  <Button
-                    color="buttoncreatenews"
-                    variant="contained"
-                    size="small"
-                    component={Link}
-                    to="/backoffice/activities/create"
-                  >
-                    Create Activity
-                  </Button>
-                </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {activities.map((activity) => (
-          <TableRow key={activity.id}>
-            <TableCell component="th" scope="row">
-              {activity.name}
-            </TableCell>
-            <TableCell align="center">
-              <img
-                className="table-row-image"
-                src={activity.image}
-                alt={activity.name}
-              />
-            </TableCell>
-            <TableCell align="center">{formatDate(new Date())}</TableCell>
-            <TableCell align="center">
-              <IconButton onClick={() => editData(activity.id)}>
-                <Edit />
-              </IconButton>
-              <IconButton
-                onClick={() => {
-                  deleteData(activity.id);
-                  newsDeleted();
-                }}
-              >
-                <Delete />
-              </IconButton>
-            </TableCell>
-          </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+        <Table className="TableFinal">
+          <TableHead className="TableRowModify">
+            <TableRow>
+              <TableCell className="TableCell"align="center">Name</TableCell>
+              <TableCell className="TableCell"align="center">Image</TableCell>
+              <TableCell className="TableCell"align="center">CreatedAt</TableCell>
+              <TableCell align="center" className="TableCell">
+                    <Button
+                      color="buttoncreatenews"
+                      variant="contained"
+                      size="small"
+                      component={Link}
+                      to="/backoffice/activities/create"
+                    >
+                      Create Activity
+                    </Button>
+                  </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {activities.map((activity) => (
+            <TableRow key={activity.id}>
+              <TableCell component="th" scope="row">
+                {activity.name}
+              </TableCell>
+              <TableCell align="center">
+                <img
+                  className="table-row-image"
+                  src={activity.image}
+                  alt={activity.name}
+                />
+              </TableCell>
+              <TableCell align="center">{formatDate(new Date())}</TableCell>
+              <TableCell align="center">
+                <IconButton onClick={() => editData(activity.id)}>
+                  <Edit />
+                </IconButton>
+                <IconButton
+                  onClick={() => deleteData(activity.id)}
+                >
+                  <Delete />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </TableContainer>
     </>
   );
