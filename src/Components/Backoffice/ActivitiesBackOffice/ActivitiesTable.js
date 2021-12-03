a08f5b8c1522ddbd03c67be30d75552902db755d
ArielSurco <arielsurcoa@gmail.com>
2021-12-02 23:29:56 -0300

    Fix activitiesTable to show the activities of API

--- src/Components/Backoffice/ActivitiesBackOffice/ActivitiesTable.js
+++ src/Components/Backoffice/ActivitiesBackOffice/ActivitiesTable.js
@@ -6,13 +6,15 @@ import { getAll, deleteById } from '../../../app/activitiesReducer/activitiesRed
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import {NewsTableRows} from '../../News/NewsTableRows';
-import {Table,TableBody,TableCell,TableContainer,TableHead,Button,TableRow} from "@mui/material";
+import {Table,TableBody,TableCell,TableContainer,TableHead,Button,TableRow,IconButton} from "@mui/material";
+import { Edit, Delete } from "@mui/icons-material";
 import TitleBackoffice from '../TitleBackoffice'
+import { formatDate } from "../../../Utils/formatters";
 const ActivitiesTable = () => {
   const history = useHistory();
-  const { activities: dataActivity } = useSelector(state => state.activities);
+  const { activities } = useSelector(state => state.activities);
   const dispatch = useDispatch();
-  const editData = (id) => history.push(`/activity-detail/${id}`);
+  const editData = (id) => history.push(`/backoffice/activities/edit/${id}`);
 
   const deleteData = (id) => {
     dispatch(deleteById(id));
@@ -47,7 +49,34 @@ const ActivitiesTable = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          <NewsTableRows newsList={dataActivity} onDelete={deleteData} onEdit={editData}/>
+          {activities.map((activity) => (
+          <TableRow key={activity.id}>
+            <TableCell component="th" scope="row">
+              {activity.name}
+            </TableCell>
+            <TableCell align="center">
+              <img
+                className="table-row-image"
+                src={activity.image}
+                alt={activity.name}
+              />
+            </TableCell>
+            <TableCell align="center">{formatDate(new Date())}</TableCell>
+            <TableCell align="center">
+              <IconButton onClick={() => editData(activity.id)}>
+                <Edit />
+              </IconButton>
+              <IconButton
+                onClick={() => {
+                  deleteData(activity.id);
+                  newsDeleted();
+                }}
+              >
+                <Delete />
+              </IconButton>
+            </TableCell>
+          </TableRow>
+          ))}
         </TableBody>
       </Table>
       </TableContainer>
